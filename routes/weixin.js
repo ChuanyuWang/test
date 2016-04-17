80f781295d40d59f2d652a5db1939479d09eed5b
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2016-04-17 11:11:47 +0800

    add validation of weixin entry

---
+++ routes/weixin.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var url = require("url");
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+	res.send('respond with a resource');
+});
+
+router.get('/check', function (req, res) {
+	var signature = req.query.signature;
+	var timestamp = req.query.timestamp;
+	var nonce = req.query.nonce;
+
+	// define token
+	var token = "Hibanana";
+	var tmpArr = new Array(token, timestamp, nonce);
+	// sort and join as string
+    var content = tmpArr.sort().join('');
+	console.log(content);
+
+    // generate sha1 
+	var crypto = require('crypto');
+	var shasum = crypto.createHash('sha1');
+	shasum.update(content);
+	var d = shasum.digest('hex');
+
+	if (signature == d) {
+		res.send(d);
+	} else {
+        res.status(403).send('Weixin authorization fails');
+    }
+});
+
+module.exports = router;
