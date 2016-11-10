7a7e06d46417e6ae9f88a65df69bfe03e640dcd4
Chuanyu Wang <chuanyu.wang@sap.com>
2016-11-10 15:34:21 +0800

    enable connection pool

--- util.js
+++ util.js
@@ -2,8 +2,20 @@ var mongojs = require('mongojs');
 var util = require('util');
 var config = require('./config.db');
 
+// Store all instantiated connections.
+var connections = [];
+
 module.exports = {
     connect : function (database) {
+        if (typeof database != "string" || database.length == 0) {
+            throw new Error("parameter databaseis not string or empty");
+        }
+        //  If a connection pool was found, return with it.
+        var db = findConnection(database, connections);
+        if (db) {
+            return db;
+        }
+        
         //https://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html
         var uriString = util.format("mongodb://%s:%s@%s/%s?authSource=admin", config.user, config.pass, config.host, database);
         var options = {
@@ -12,13 +24,21 @@ module.exports = {
                 authSource : 'admin'
             },
             server : {
-                poolSize : 3,
+                poolSize : 5
+                /*,
                 socketOptions : {
                     keepAlive : 120
                 }
+                */
             }
         };
-        var db = mongojs(uriString);
+        var db = mongojs(uriString, [], options);
+        
+        // Store the connection in the connections array.
+        connections.push({
+            name : database,
+            db : db
+        });
 
         db.on('error', function (err) {
             console.error('connect database "%s" with error', db, err);
@@ -29,4 +49,13 @@ module.exports = {
         })
         return db;
     }
-}
+};
+
+function findConnection(databaseName, connections) {
+    for (var i=0;i<connections.length;i++) {
+        if (databaseName == connections[i].name) {
+            return connections[i].db;
+        }
+    }
+    return null;
+};
