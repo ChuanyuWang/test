ca7a4852a292efad16ed8ee5d45bf56c8ee1d6e2
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2016-12-30 22:57:05 +0800

    format code style with visual studio code

--- routes/base.js
+++ routes/base.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 router.get('/home', checkTenantUser, function (req, res) {
     res.render('bqsq/home', {
-        title : '课程表',
-        user : req.user,
-        navTitle : req.tenant.displayName,
-        classroom : req.tenant.classroom ? req.tenant.classroom : []
+        title: '课程表',
+        user: req.user,
+        navTitle: req.tenant.displayName,
+        classroom: req.tenant.classroom ? req.tenant.classroom : []
     });
 });
 
@@ -14,46 +14,45 @@ router.get('/member', checkTenantUser, function (req, res) {
     var members = req.db.collection("members");
     //TODO, support multi membership card
     members.aggregate([{
-                $match : {
-                    membership : {
-                        $size : 1
-                    },
-                    "membership.0.expire" : {
-                        $gt : new Date()
-                    },
-                    "membership.0.credit" : {
-                        $gt : 0
-                    }
-                }
-            }, {
-                $unwind : "$membership"
-            }, {
-                $group : {
-                    _id : null,
-                    count : {
-                        $sum : 1 // the count of valid member who is not expire and has remaining credit
-                    },
-                    total : {
-                        $sum : "$membership.credit" // the total of valid members' remaining credit
-                    }
-                }
+        $match: {
+            membership: {
+                $size: 1
+            },
+            "membership.0.expire": {
+                $gt: new Date()
+            },
+            "membership.0.credit": {
+                $gt: 0
             }
-        ], function (err, docs) {
+        }
+    }, {
+        $unwind: "$membership"
+    }, {
+        $group: {
+            _id: null,
+            count: {
+                $sum: 1 // the count of valid member who is not expire and has remaining credit
+            },
+            total: {
+                $sum: "$membership.credit" // the total of valid members' remaining credit
+            }
+        }
+    }], function (err, docs) {
         if (err)
             console.error(err);
 
-        var doc = {count:0, total:0};
+        var doc = { count: 0, total: 0 };
         if (docs && docs.length == 1)
             doc = docs[0];
 
         res.render('bqsq/member', {
-            title : '会员',
-            user : req.user,
-            navTitle : req.tenant.displayName,
-            classroom : req.tenant.classroom ? req.tenant.classroom : [],
-            statistics : {
-                count : doc.count,
-                total : Math.round(doc.total*10)/10
+            title: '会员',
+            user: req.user,
+            navTitle: req.tenant.displayName,
+            classroom: req.tenant.classroom ? req.tenant.classroom : [],
+            statistics: {
+                count: doc.count,
+                total: Math.round(doc.total * 10) / 10
             }
         });
     });
@@ -61,36 +60,36 @@ router.get('/member', checkTenantUser, function (req, res) {
 
 router.get('/opportunity', checkTenantUser, function (req, res) {
     res.render('bqsq/opportunity', {
-        title : '试听',
-        user : req.user,
-        navTitle : req.tenant.displayName
+        title: '试听',
+        user: req.user,
+        navTitle: req.tenant.displayName
     });
 });
 
 router.get('/setting', checkTenantUser, function (req, res) {
     res.render('bqsq/setting', {
-        title : '设置',
-        user : req.user,
-        navTitle : req.tenant.displayName
+        title: '设置',
+        user: req.user,
+        navTitle: req.tenant.displayName
     });
 });
 
 router.get('/mybooking', function (req, res) {
     res.render('bqsq/mybooking', {
-        title : '我的课程',
-        classroom : JSON.stringify(req.tenant.classroom ? req.tenant.classroom : [])
+        title: '我的课程',
+        classroom: JSON.stringify(req.tenant.classroom ? req.tenant.classroom : [])
     });
 });
 
 router.get('/myReadBooks', function (req, res) {
     res.render('bqsq/myReadBooks', {
-        title : '我的英文绘本'
+        title: '我的英文绘本'
     });
 });
 
 router.get('/trial', function (req, res) {
     res.render('bqsq/trial', {
-        title : '报名试听'
+        title: '报名试听'
     });
 });
 
