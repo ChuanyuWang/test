0413ea01ea807432bf49ca024111a7c5319b158c
Chuanyu Wang <chuanyu.wang@sap.com>
2017-02-28 17:43:57 +0800

    add dummy respod of api courses get

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -8,8 +8,27 @@ var NORMAL_FIELDS = {
 
 // Below APIs are visible to anonymous users
 
-router.get('/', function (req, res, next) {
-    return next(new Error("Not implementation"));
+router.get('/', function(req, res, next) {
+    //return next(new Error("Not implementation"));
+    res.json([
+        {
+            "_id": "1",
+            "name": "123",
+            "date": "2017-02-27T02:00:00Z",
+            "classroom": "room1",
+            "members": [
+                { "id": "123", "name": "Hellen" },
+                { "id": "456", "name": "Peter" }
+            ],
+            "recurrence": {
+                "pattern": "weekly" | "daily",
+                "iterator": 1,
+                "matcher": ["monday", "sunday"],
+                "start": "2017-02-27T02:00:00Z",
+                "end": 12 | "2017-02-27T02:00:00Z"
+            }
+        }
+    ]);
 });
 
 // Below APIs are visible to authenticated users by current tenant
@@ -18,15 +37,15 @@ router.all(helper.isAuthenticated);
 // Below APIs are only visible to authenticated users with 'admin' role
 router.all(helper.requireRole("admin"));
 
-router.post('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
     return next(new Error("Not implementation"));
 });
 
-router.patch('/:courseID', function (req, res, next) {
+router.patch('/:courseID', function(req, res, next) {
     return next(new Error("Not implementation"));
 });
 
-router.delete ('/:courseID', function (req, res, next) {
+router.delete('/:courseID', function(req, res, next) {
     return next(new Error("Not implementation"));
 });
 
