b70ab93b7fcd199baf0e3a773bbe2ca513b5e9ee
Chuanyu Wang <chuanyu.wang@sap.com>
2017-03-02 16:58:31 +0800

    add event handler for table in line actions

--- src/js/course.js
+++ src/js/course.js
@@ -17,3 +17,49 @@ function init() {
     moment.locale('zh-CN');
     bootbox.setLocale('zh_CN');
 };
+
+/**
+ * event handler of clicking end course button 
+ * 
+ * @param {Object} e the jQuery event
+ * @param {Object} value the field value
+ * @param {Object} row the row record data
+ * @param {Number} index the row index
+ * @public
+ */
+function handleEndCourse(e, value, row, index) {
+    //TODO
+};
+
+/**
+ * event handler of clicking end course button 
+ * 
+ * @param {Object} e the jQuery event
+ * @param {Object} value the field value
+ * @param {Object} row the row record data
+ * @param {Number} index the row index
+ * @public
+ */
+function handleEditCourse(e, value, row, index) {
+    //TODO
+};
+
+/**
+ * event handler of clicking end course button 
+ * 
+ * @param {Object} e the jQuery event
+ * @param {Object} value the field value
+ * @param {Object} row the row record data
+ * @param {Number} index the row index
+ * @public
+ */
+function handleViewCourse(e, value, row, index) {
+    //TODO
+};
+
+// global event handler for table inline action
+handleActionClick = {
+    'click .end': handleEndCourse,
+    'click .edit': handleEditCourse,
+    'click .view': handleViewCourse,
+};
