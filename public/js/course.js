8eeac56cc7a259512c3ac27ed81cfa2dd988a8ec
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2017-04-17 00:50:43 +0800

    refine table on course page

--- public/js/course.js
+++ public/js/course.js
@@ -2,12 +2,49 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * --------------------------------------------------------------------------
+ * common.js
+ * --------------------------------------------------------------------------
+ */
+
+module.exports = {
+    /**
+     * get the tenat name of current page, e.g.
+     * return 'bqsq' from http://localhost:3000/t/bqsq/course/1/view
+     */
+    getTenantName: function() {
+        var pathname = window.location.pathname;
+        if (pathname.length == 0) return "";
+        if (pathname.charAt(0) == '/') pathname = pathname.substring(1);
+        if (pathname.charAt(0) == 't') pathname = pathname.substring(1);
+        if (pathname.charAt(0) == '/') pathname = pathname.substring(1);
+        return pathname.split('/')[0];
+    },
+    /**
+     * Data fomatter function of bootstrap-table to format date localized string by 'll'
+     * 
+     * @param {Object} value the field value
+     * @param {Object} row the row record data
+     * @param {Number} index the row index
+     */
+    dateFormatter: function(value, row, index) {
+        if (value) {
+            return moment(value).format('ll');
+        } else {
+            return undefined;
+        }
+    }
+};
+},{}],2:[function(require,module,exports){
+/**
+ * --------------------------------------------------------------------------
  * course.js
  * --------------------------------------------------------------------------
  */
 
+var common = require('./common');
+
 // DOM Ready =============================================================
-$(document).ready(function () {
+$(document).ready(function() {
     init();
 });
 
@@ -18,6 +55,20 @@ function init() {
     //TODO, localization 
     moment.locale('zh-CN');
     bootbox.setLocale('zh_CN');
+
+    $('#course_table').bootstrapTable({
+        locale: 'zh-CN',
+        columns: [{}, {
+            formatter: common.dateFormatter
+        }, {
+            formatter: statusFormatter
+        }, {
+            formatter: membersFormatter
+        }, {
+            formatter: actionFormatter,
+            events: handleActionClicks
+        }]
+    });
 };
 
 /**
@@ -60,10 +111,34 @@ function handleViewCourse(e, value, row, index) {
 };
 
 // global event handler for table inline action
-handleActionClick = {
+var handleActionClicks = {
     'click .end': handleEndCourse,
     'click .edit': handleEditCourse,
     'click .view': handleViewCourse,
 };
 
-},{}]},{},[1]);
+function membersFormatter(value, row, index) {
+    return value && value.length ? value.length : 0;
+};
+
+function statusFormatter(value, row, index) {
+    if (value == "inprogress") return '进行中';
+    if (value == "closed") return '结束';
+    return value;
+};
+
+function actionFormatter(value, row, index) {
+    return [
+        '<button type="button" class="end btn btn-danger btn-xs">',
+        '  <span class="glyphicon glyphicon-ban-circle"></span> 结束',
+        '</button>',
+        '<button type="button" style="margin:0px 3px" class="edit btn btn-warning btn-xs">',
+        '  <span class="glyphicon glyphicon-edit"></span> 修改',
+        '</button>',
+        '<button type="button" class="view btn btn-primary btn-xs">',
+        '  <span class="glyphicon glyphicon-expand"></span> 查看',
+        '</button>'
+    ].join('');
+};
+
+},{"./common":1}]},{},[2]);
--- src/js/course.js
+++ src/js/course.js
@@ -4,8 +4,10 @@
  * --------------------------------------------------------------------------
  */
 
+var common = require('./common');
+
 // DOM Ready =============================================================
-$(document).ready(function () {
+$(document).ready(function() {
     init();
 });
 
@@ -16,6 +18,20 @@ function init() {
     //TODO, localization 
     moment.locale('zh-CN');
     bootbox.setLocale('zh_CN');
+
+    $('#course_table').bootstrapTable({
+        locale: 'zh-CN',
+        columns: [{}, {
+            formatter: common.dateFormatter
+        }, {
+            formatter: statusFormatter
+        }, {
+            formatter: membersFormatter
+        }, {
+            formatter: actionFormatter,
+            events: handleActionClicks
+        }]
+    });
 };
 
 /**
@@ -58,8 +74,32 @@ function handleViewCourse(e, value, row, index) {
 };
 
 // global event handler for table inline action
-handleActionClick = {
+var handleActionClicks = {
     'click .end': handleEndCourse,
     'click .edit': handleEditCourse,
     'click .view': handleViewCourse,
+};
+
+function membersFormatter(value, row, index) {
+    return value && value.length ? value.length : 0;
+};
+
+function statusFormatter(value, row, index) {
+    if (value == "inprogress") return '进行中';
+    if (value == "closed") return '结束';
+    return value;
+};
+
+function actionFormatter(value, row, index) {
+    return [
+        '<button type="button" class="end btn btn-danger btn-xs">',
+        '  <span class="glyphicon glyphicon-ban-circle"></span> 结束',
+        '</button>',
+        '<button type="button" style="margin:0px 3px" class="edit btn btn-warning btn-xs">',
+        '  <span class="glyphicon glyphicon-edit"></span> 修改',
+        '</button>',
+        '<button type="button" class="view btn btn-primary btn-xs">',
+        '  <span class="glyphicon glyphicon-expand"></span> 查看',
+        '</button>'
+    ].join('');
 };
