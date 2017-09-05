94760ad82ab3ad7aad1a8eaef2f14e25badda0c7
Chuanyu Wang <chuanyu.wang@sap.com>
2017-09-05 15:06:38 +0800

    add validation for add multi classes dialog

--- src/js/components/add-multi-class-modal.js
+++ src/js/components/add-multi-class-modal.js
@@ -15,12 +15,13 @@ module.exports = {
             begin: moment(),
             end: moment(),
             room: '',
+            cost: 0,
             weekdays: [],
             isRepeated: false
         };
     },
     watch: {
-        isRepeated : function(newValue) {
+        isRepeated: function(newValue) {
             var vm = this;
             if (newValue) {
                 $(vm.$el).find('#class_date').data("DateTimePicker").format('LT');
@@ -29,32 +30,52 @@ module.exports = {
             }
         }
     },
-    computed: {},
+    computed: {
+        validation: function() {
+            return {
+                cost: typeof (this.cost) === 'number' && this.cost >= 0,
+                date: this.date && this.date.isValid(),
+                room: typeof(this.room) === 'string' && this.room.length > 0,
+                weekdays: !this.isRepeated || this.weekdays.length > 0,
+                begin: !this.isRepeated || (this.begin && this.begin.isValid()),
+                end: !this.isRepeated || (this.end && this.end.isValid())
+            }
+        },
+        isValid: function() {
+            var validation = this.validation
+            return Object.keys(validation).every(function(key) {
+                return validation[key]
+            })
+        }
+    },
     filters: {},
     methods: {
         show: function(value) {
             // TODO, clear error
             $('#add-multi-class-modal').modal('show')
         },
-        handleOK : function() {
-            console.log(this.date.toString());
-            console.log(this.weekdays);
-            this.$emit("ok", this.$data);
-            $('#add-multi-class-modal').modal('hide')
+        handleOK: function() {
+            if (this.isValid) {
+                this.$emit("ok", this.$data);
+                $('#add-multi-class-modal').modal('hide');
+            }
         }
     },
     mounted: function() {
         // 'this' is refer to vm instance
         var vm = this;
         $(vm.$el).find('#class_date').datetimepicker({
+            defaultDate: moment(),
             locale: 'zh-CN',
             format: 'lll'
         });
         $(vm.$el).find('#class_begin').datetimepicker({
+            defaultDate: moment(),
             locale: 'zh-CN',
             format: 'll'
         });
         $(vm.$el).find('#class_end').datetimepicker({
+            defaultDate: moment().add(1, 'week'),
             locale: 'zh-CN',
             format: 'll'
         });
