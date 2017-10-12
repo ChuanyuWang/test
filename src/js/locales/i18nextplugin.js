ec5918c80bf6eb4eb84d516c2f2b90772ed211ce
Chuanyu Wang <chuanyu.wang@sap.com>
2017-10-12 11:07:03 +0800

    set lang as 'zh' for i18next

--- src/js/locales/i18nextplugin.js
+++ src/js/locales/i18nextplugin.js
@@ -14,14 +14,16 @@ var resources = {
 var i18nextPlugin = {
     install: function(Vue, options) {
         if (i18next) {
+            // more options refer to https://www.i18next.com/configuration-options.html
             i18next.use(i18nextBrowserLanguageDetector).init({
-                fallbackLng: "zh",
+                lng: "zh", // remove this line to enable auto language detection
+                fallbackLng: "en",
                 resources: resources,
                 detection: { lookupQuerystring: 'lang' }
             }, function(err, t) {
                 if (err) return console.error('init i18next with error' + err);
                 Vue.i18next = i18next; // append the global function 'i18next' to Vue
-                Vue.prototype.$t = t; // an instance method 't'
+                Vue.prototype.$t = t; // add method 't' to Vue instance
             });
         } else {
             console.error('i18next is not found');
