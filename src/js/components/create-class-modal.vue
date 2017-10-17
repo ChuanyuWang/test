0f063855b9b8b800692fbd98acac5461f3b422c3
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2017-10-17 23:48:08 +0800

    convert create class dialog as vue component

---
+++ src/js/components/create-class-modal.vue
@@ -0,0 +1,140 @@
+<style>
+
+</style>
+
+<template lang="jade">
+div.modal.fade(tabindex='-1',role='dialog',data-backdrop='static')
+  div.modal-dialog(role="document")
+    div.modal-content
+      div.modal-header
+        button.close(type="button",data-dismiss="modal",aria-label="Close")
+          span(aria-hidden="true") &times
+        h4.modal-title 添加常规课程
+      div.modal-body
+        form.form-horizontal
+          div.form-group(:class='{"has-error": errors.name}',:title='errors.name')
+            label.control-label.col-sm-2(for='cls_name') 课程名称:
+            div.col-sm-10
+              input#cls_name.form-control(type='text',placeholder='课程描述',autofocus,v-model='name')
+          div.form-group(:class='{"has-error": errors.cost}',:title='errors.cost')
+            label.control-label.col-sm-2(for='cost') 所需课时:
+            div.col-sm-2
+              input.form-control(type='number',name='cost',min='0',step='0.1',v-model.number='cost')
+          div.form-group
+            label.control-label.col-sm-2 时间:
+            div.col-sm-10
+              p.form-control-static(style="width:110px;float:left") {{classDate}}
+              date-picker(style="width:150px;float:left",v-model='startTime',:config='{locale: "zh-CN", format: "LT"}')
+          div.form-group(:class='{"has-error": errors.age}',:title='errors.age')
+            label.control-label.col-sm-2 年龄:
+            div.col-sm-10(style="display:inline-flex")
+              input.form-control(type='number',min='0',style={'width':'60px'},v-model.number='age.min')
+              p.form-control-static(style={'display':'inline-block','margin-left':'3px','float':'left'}) 至
+              input.form-control(type='number',min='0',style={'width':'60px','margin-left':'3px'},v-model.number='age.max')
+              p.form-control-static(style={'display':'inline-block','margin-left':'3px'}) 月
+          div.form-group(:class='{"has-error": errors.capacity}',:title='errors.capacity')
+            label.control-label.col-sm-2(for='cls_capacity') 最大人数:
+            div.col-sm-2
+              input#cls_capacity.form-control(type='number',min='0',v-model.number='capacity')
+      div.modal-footer
+        p.small(style='color:#777;float:left;margin-top:7px') *如果需要添加课程到指定班级，请到班级设置中添加
+        button.btn.btn-default(type="button",data-dismiss="modal") {{$t('dialog_cancel')}}
+        button#create_cls.btn.btn-success(type="button",v-on:click='handleOK',:disabled='hasError') 添加
+</template>
+
+<script>
+/**
+ * --------------------------------------------------------------------------
+ * create-class-modal.vue component display a dailog to create class
+ * --------------------------------------------------------------------------
+ */
+
+var date_picker = require('./date-picker.vue');
+
+module.exports = {
+  components: {
+    'date-picker': date_picker
+  },
+  props: {},
+  data: function() {
+    return {
+      startDate: moment(),
+      startTime: moment(),
+      name: '',
+      cost: 1,
+      capacity: 8,
+      age: {
+        min: 24,
+        max: 48
+      },
+      classroom: null
+    };
+  },
+  watch: {
+    "age.min": function(value) {
+      this.age.min = parseInt(value);
+    },
+    "age.max": function(value) {
+      this.age.max = parseInt(value);
+    },
+    capacity: function(value) {
+      this.capacity = parseInt(value);
+    }
+  },
+  computed: {
+    classDate: function() {
+      return this.startDate.format('ll');
+    },
+    errors: function() {
+      var errors = {};
+      if (!this.name || this.name.length == 0)
+        errors.name = '课程名称不能为空';
+      if (this.cost === '' || this.cost < 0)
+        errors.cost = '所需课时不能为负';
+      if (isNaN(this.capacity) || this.capacity < 0)
+        errors.capacity = '最大人数不能为负';
+      if (this.age.min < 0)
+        errors.age = '最小年龄不能为负';
+      if (this.age.max < 0)
+        errors.age = '最大年龄不能为负';
+      if (this.age.max < this.age.min)
+        errors.age = '最大年龄不能小于最小年龄';
+      return errors;
+    },
+    hasError: function() {
+      var errors = this.errors
+      return Object.keys(errors).some(function(key) {
+        return true;
+      })
+    }
+  },
+  filters: {},
+  methods: {
+    show: function(classTime, classroom) {
+      this.name = '';
+      this.startDate = moment(classTime);
+      this.startTime = moment(classTime);
+      this.classroom = classroom;
+      $(this.$el).modal('show');
+    },
+    handleOK: function() {
+      if (this.hasError) return;
+
+      var createdClass = {
+        name: this.name,
+        // get the date from text control and appending the time
+        date: moment(this.startDate).hours(this.startTime.hours()).minutes(this.startTime.minutes()),
+        cost: this.cost,
+        capacity: this.capacity,
+        classroom: this.classroom,
+        age: this.age
+      };
+      this.$emit("ok", createdClass);
+      $(this.$el).modal('hide');
+    }
+  },
+  mounted: function() {
+    //var vm = this;
+  }
+};
+</script>
--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,9 @@
  * --------------------------------------------------------------------------
  */
 var common = require('./common');
-var locales = require('./locales');
+var i18nextplugin = require('./locales/i18nextplugin');
 var classList = require('./components/class-list.vue');
 var class_service = require('./services/classes');
-var LngDetector = require('./locales/i18nextBrowserLanguageDetector.min');
 
 var classTableData = {
     monday: null, // moment date object
@@ -22,10 +21,11 @@ $(document).ready(function() {
     
     // bootstrap the class table
     new Vue({
-        el: '#cls_table',
+        el: '#cls_app',
         data: classTableData,
         components: {
-            'class-list': classList
+            'class-list': classList,
+            'create-class-modal': require('./components/create-class-modal.vue')
         },
         computed: {
             sortedClasses: function() {
@@ -110,17 +110,28 @@ $(document).ready(function() {
             },
             addClass: function(dayOffset, startTime) {
                 // the first class should start from 8:00 in the morning
-                showAddNewClassDlg(moment(this.monday).add(dayOffset, 'days').hours(startTime == 0 ? 8 : startTime));
+                this.$refs.createClsDlg.show(moment(this.monday).add(dayOffset, 'days').hours(startTime == 0 ? 8 : startTime), getCurrentClassRoom());
+            },
+            createClass: function(classItem) {
+                // TODO
+                $.ajax("/api/classes", {
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    data: JSON.stringify(classItem),
+                    success: function(data) {
+                        updateClasses(data);
+                        //modal.modal('hide');
+                        // jump to new class page
+                        //window.location.href = './class/' + data._id;
+                    },
+                    error: function(jqXHR, status, err) {
+                        showErrorMsg(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
+                    },
+                    dataType: "json"
+                });
             }
         }
     });
-
-    $('#cls_dlg').on('shown.bs.modal', function(event) {
-        $(this).find('#cls_name').focus(); // focus on the class name input control
-    });
-
-    // listen to the action button on modal dialogs
-    $('#create_cls').click(handleAddNewClass);
 
     // listen to the previous week and next week button
     $('#previous_week').click(function(event) {
@@ -151,21 +162,11 @@ $(document).ready(function() {
 
 function init() {
     console.log("welcome~~~");
-    // initialize the i18n module
-    if (i18next) {
-        i18next.use(LngDetector).init({
-            fallbackLng: "en",
-            resources: locales,
-            detection: {lookupQuerystring:'lang'}
-        });
-    }
+    // load the i18next plugin to Vue
+    Vue.use(i18nextplugin);
 
     moment.locale('zh-CN');
     bootbox.setLocale('zh_CN');
-    $('#cls_time').datetimepicker({
-        locale: 'zh-CN',
-        format: 'LT'
-    });
     $('#weekPicker').datetimepicker({
         showTodayButton: false,
         locale: 'zh-CN',
@@ -218,105 +219,6 @@ function getMonday(date) {
     //set the time to the very beginning of day
     _date.hours(0).minutes(0).seconds(0).milliseconds(0);
     return _date;
-}
-
-function showAddNewClassDlg(startDateTime) {
-    // reset dialog status when add a new class
-    var modal = $('#cls_dlg');
-
-    modal.find('#cls_name').val("").closest(".form-group").removeClass("has-error");
-    modal.find('input[name=cost]').val(1).closest(".form-group").removeClass("has-error");
-    modal.find('#cls_capacity').val(8).closest(".form-group").removeClass("has-error");
-    modal.find('input[name=age_min]').val(24).closest(".form-group").removeClass("has-error");
-    modal.find('input[name=age_max]').val(48).closest(".form-group").removeClass("has-error");
-    modal.find('#cls_date').text(startDateTime.format('ll'));
-    modal.find('#cls_time').data('DateTimePicker').date(startDateTime);
-    modal.modal('show');
-}
-
-function handleAddNewClass(event) {
-    var modal = $(this).closest('.modal');
-    var hasError = false;
-    // validate the input
-    var classItem = {};
-    classItem.name = modal.find('#cls_name').val();
-    if (!classItem.name || classItem.name.length == 0) {
-        modal.find('#cls_name').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('#cls_name').closest(".form-group").removeClass("has-error");
-    }
-    // get date/time
-    classItem.date = moment(modal.find('#cls_date').text(), 'll');
-    var time = modal.find('#cls_time').data("DateTimePicker").date();
-    classItem.date.hours(time.hours());
-    classItem.date.minutes(time.minutes());
-    // get cost
-    classItem.cost = parseFloat(modal.find('input[name=cost]').val());
-    if (isNaN(classItem.cost) || classItem.cost < 0) {
-        modal.find('input[name=cost]').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('input[name=cost]').closest(".form-group").removeClass("has-error");
-    }
-    // get capacity
-    classItem.capacity = parseInt(modal.find('#cls_capacity').val());
-    if (isNaN(classItem.capacity) || classItem.capacity < 0) {
-        modal.find('#cls_capacity').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('#cls_capacity').closest(".form-group").removeClass("has-error");
-    }
-    // get age limitation
-    classItem.age = { min: null, max: null };
-    var input = modal.find('input[name=age_min]');
-    if (input.val()) {
-        classItem.age.min = parseInt(input.val());
-        if (isNaN(classItem.age.min) || classItem.age.min < 0) {
-            input.closest(".form-group").addClass("has-error");
-            hasError = true;
-        } else {
-            input.closest(".form-group").removeClass("has-error");
-        }
-    }
-
-    var input = modal.find('input[name=age_max]');
-    if (input.val()) {
-        classItem.age.max = parseInt(input.val());
-        if (isNaN(classItem.age.max) || classItem.age.max < 0) {
-            input.closest(".form-group").addClass("has-error");
-            hasError = true;
-        } else {
-            input.closest(".form-group").removeClass("has-error");
-        }
-    }
-
-    // get classroom
-    classItem.classroom = getCurrentClassRoom();
-
-    if (!hasError) {
-        // switch the age.min and age.max if min is bigger than max
-        if (classItem.age.min != null && classItem.age.max != null && classItem.age.min > classItem.age.max) {
-            var temp = classItem.age.max;
-            classItem.age.max = classItem.age.min;
-            classItem.age.min = temp;
-        }
-        $.ajax("/api/classes", {
-            type: "POST",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(classItem),
-            success: function(data) {
-                updateClasses(data);
-                modal.modal('hide');
-                // jump to new class page
-                //window.location.href = './class/' + data._id;
-            },
-            error: function(jqXHR, status, err) {
-                showErrorMsg(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
-            },
-            dataType: "json"
-        });
-    }
 }
 
 function updateClasses(newClass) {
