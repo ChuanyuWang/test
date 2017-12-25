60d55ecf45cbd0249b29462522844fc5ed8a69b3
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2017-12-25 10:44:01 +0800

    add util connect function for mongoose

--- account.js
+++ account.js
@@ -1,38 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
-var config = require('./config.db');
-var util = require("util");
-
-// Use native promises, more refer to http://mongoosejs.com/docs/promises.html
-mongoose.Promise = global.Promise;
+var util = require("./util");
 
 //the uri of 'config' database
-var uri = util.format("mongodb://%s/config?authSource=admin", config.host);
-var options = {
-    db : {
-        native_parser : true,
-        authSource : 'admin'
-    },
-    server : {
-        poolSize : 3,
-        socketOptions : {
-            keepAlive : 120
-        }
-    },
-    user : config.user,
-    pass : config.pass
-};
-var conn = mongoose.createConnection(uri, options);
+var conn = util.connect2("config");
 
 var Account = new Schema({
-        username : String,
-        password : String,
-        tenant : String,
-        displayName : String,
-        role : String
-    });
+    username: String,
+    password: String,
+    tenant: String,
+    displayName: String,
+    role: String
+});
 
 Account.plugin(passportLocalMongoose);
 
-module.exports = conn.model('account', Account);
+module.exports = conn.model('Account', Account);
--- util.js
+++ util.js
@@ -1,9 +1,14 @@
 var mongojs = require('mongojs');
 var util = require('util');
 var config = require('./config.db');
+var mongoose = require('mongoose');
+
+// Use native promises, more refer to http://mongoosejs.com/docs/promises.html
+mongoose.Promise = global.Promise;
 
 // Store all instantiated connections.
 var connections = [];
+var connections2 = {};
 
 // export helper functions
 var helpers = {};
@@ -13,7 +18,7 @@ helpers.connectionURI = function(database) {
     return util.format("mongodb://%s:%s@%s/%s", config.user, config.pass, config.host, database);
 };
 
-helpers.connect = function (database) {
+helpers.connect = function(database) {
     if (typeof database != "string" || database.length == 0) {
         throw new Error("parameter databaseis not string or empty");
     }
@@ -22,34 +27,75 @@ helpers.connect = function (database) {
     if (db) {
         return db;
     }
-    
+
     //https://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html
     var uriString = util.format("mongodb://%s:%s@%s/%s", config.user, config.pass, config.host, database);
     var options = {
-        authSource : 'admin',
-        poolSize : 5
+        authSource: 'admin',
+        poolSize: 5
         /* keepAlive : 120 */
     };
     var db = mongojs(helpers.connectionURI(database), [], options);
-    
+
     // Store the connection in the connections array.
     connections.push({
-        name : database,
-        db : db
+        name: database,
+        db: db
     });
 
-    db.on('error', function (err) {
+    db.on('error', function(err) {
         console.error('connect database "%s" with error', db.toString(), err);
-        state.db = null;
+        // TODO, remove error db from connection pool
     })
-    db.on('connect', function () {
+    db.on('connect', function() {
         console.log('database "%s" is connected', db.toString());
     })
     return db;
 };
 
+/**
+ * 
+ * @param {String} database name of database to be connected
+ * @returns connection created by `mongoose.createConnection`
+ */
+helpers.connect2 = function(database) {
+    if (typeof database != "string" || database.length == 0) {
+        throw new Error("parameter databaseis not string or empty");
+    }
+    //  If a connection was found, return with it.
+    if (connections2[database]) {
+        return connections2[database];
+    }
+
+    var options = {
+        useMongoClient: true,
+        //keepAlive: 120,
+        autoIndex: false, // Don't build indexes
+        reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+        reconnectInterval: 500, // Reconnect every 500ms
+        poolSize: 5, // Maintain up to 10 socket connections
+        // If not connected, return errors immediately rather than waiting for reconnect
+        bufferMaxEntries: 0,
+        authSource: 'admin'
+    };
+
+    //https://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html
+    var uriString = util.format("mongodb://%s:%s@%s/%s", config.user, config.pass, config.host, database);
+    var conn = mongoose.createConnection(uriString, options);
+
+    // catch error and remove failure connection
+    conn.catch(function(err) {
+        console.error('connect database "%s" with error', database, err);
+        delete connections2[database];
+    });
+
+    // Store the connection in the connections pool.
+    connections2[database] = conn;
+    return conn;
+};
+
 function findConnection(databaseName, connections) {
-    for (var i=0;i<connections.length;i++) {
+    for (var i = 0; i < connections.length; i++) {
         if (databaseName == connections[i].name) {
             return connections[i].db;
         }
