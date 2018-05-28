bf4689493e0dfb0ebd87c3d96dd78143bc6b43ef
Chuanyu Wang <chuanyu.wang@sap.com>
2018-05-28 13:57:33 +0800

    change the CDN back to bootcss provider

--- config.js
+++ config.js
@@ -15,22 +15,22 @@ module.exports = {
      * 4. http://www.bootcdn.cn/ (reject .cc .top)
      */
     cdnlibs: {
-        jquery: '//cdn.staticfile.org/jquery/3.2.1/jquery.min.js',
-        jquery_dev: '//cdn.staticfile.org/jquery/3.2.1/jquery.js',
-        bootstrap: '//ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/bootstrap.min.js',
-        bootstrap_css: '//ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/css/bootstrap.css',
-        bootstrap_dev: '//ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/bootstrap.js',
-        momentjs: '//cdn.staticfile.org/moment.js/2.18.1/moment-with-locales.min.js',
-        momentjs_dev: '//cdn.staticfile.org/moment.js/2.18.1/moment-with-locales.js',
-        vue: '//cdnjs.cloudflare.com/ajax/libs/vue/2.5.16/vue.min.js',
-        vue_dev: '//cdnjs.cloudflare.com/ajax/libs/vue/2.5.16/vue.js',
-        i18next: '//cdnjs.cloudflare.com/ajax/libs/i18next/11.3.2/i18next.min.js',
-        i18next_dev: '//cdnjs.cloudflare.com/ajax/libs/i18next/11.3.2/i18next.js',
-        bootstrap_table_css: '//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.12.1/bootstrap-table.min.css',
-        bootstrap_table: '//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.12.1/bootstrap-table.min.js',
-        bootstrap_table_locale_zh_CN: '//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.12.1/locale/bootstrap-table-zh-CN.min.js',
-        bootstrap_datetimepicker: '//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js',
-        bootstrap_datetimepicker_css: '//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css',
-        bootbox: '//cdn.staticfile.org/bootbox.js/4.4.0/bootbox.min.js'
+        jquery: '//cdn.bootcss.com/jquery/3.3.1/jquery.min.js',
+        jquery_dev: '//cdn.bootcss.com/jquery/3.3.1/jquery.js',
+        bootstrap: '//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js',
+        bootstrap_css: '//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css',
+        bootstrap_dev: '//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.js',
+        momentjs: '//cdn.bootcss.com/moment.js/2.22.1/moment-with-locales.min.js',
+        momentjs_dev: '//cdn.bootcss.com/moment.js/2.22.1/moment-with-locales.js',
+        vue: '//cdn.bootcss.com/vue/2.5.16/vue.min.js',
+        vue_dev: '//cdn.bootcss.com/vue/2.5.16/vue.js',
+        i18next: '//cdn.bootcss.com/i18next/11.3.2/i18next.min.js',
+        i18next_dev: '//cdn.bootcss.com/i18next/11.3.2/i18next.js',
+        bootstrap_table_css: '//cdn.bootcss.com/bootstrap-table/1.12.1/bootstrap-table.min.css',
+        bootstrap_table: '//cdn.bootcss.com/bootstrap-table/1.12.1/bootstrap-table.min.js',
+        bootstrap_table_locale_zh_CN: '//cdn.bootcss.com/bootstrap-table/1.12.1/locale/bootstrap-table-zh-CN.min.js',
+        bootstrap_datetimepicker: '//cdn.bootcss.com/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js',
+        bootstrap_datetimepicker_css: '//cdn.bootcss.com/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css',
+        bootbox: '//cdn.bootcss.com/bootbox.js/4.4.0/bootbox.min.js'
     }
 };
