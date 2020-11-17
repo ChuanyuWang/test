faef5f0fd90cf624d6e27da1e112ee664bd06136
Chuanyu Wang <chuanyu.wang@sap.com>
2020-11-17 11:51:14 +0800

    refine teacher router

--- routes/api/teachers.js
+++ routes/api/teachers.js
@@ -66,12 +66,8 @@ router.patch('/:teacherID', helper.requireRole("admin"), function(req, res, next
     delete req.body._id;
 
     teachers.findAndModify({
-        query: {
-            _id: mongoist.ObjectId(req.params.teacherID)
-        },
-        update: {
-            $set: req.body
-        },
+        query: { _id: mongoist.ObjectId(req.params.teacherID) },
+        update: { $set: req.body },
         fields: NORMAL_FIELDS,
         new: true
     }).then(function(updatedDoc) {
@@ -90,42 +86,31 @@ router.patch('/:teacherID', helper.requireRole("admin"), function(req, res, next
     });
 });
 
-router.delete('/:teacherID', helper.requireRole("admin"), function(req, res, next) {
-    const db = dbUtility.connect4(req.tenant.name);
-    const classes = db.collection('classes');
-    classes.findOne({ teacher: mongoist.ObjectId(req.params.teacherID) }, { 'name': 1 }).then(function(doc) {
-        let teachers = db.collection("teachers");
+router.delete('/:teacherID', helper.requireRole("admin"), async function(req, res, next) {
+    try {
+        const db = dbUtility.connect4(req.tenant.name);
+        const doc = await db.classes.findOne({ teacher: mongoist.ObjectId(req.params.teacherID) }, { 'name': 1 });
         if (doc) {
             // set the teacher's status as 'deleted' if any class exists
-            return teachers.findAndModify({
-                query: {
-                    _id: mongoist.ObjectId(req.params.teacherID)
-                },
-                update: {
-                    $set: { 'status': "deleted" }
-                },
+            const deletedOne = await db.teachers.findAndModify({
+                query: { _id: mongoist.ObjectId(req.params.teacherID) },
+                update: { $set: { 'status': "deleted" } },
                 fields: NORMAL_FIELDS,
                 new: true
-            }).then(function(doc) {
-                console.log("teacher %s is marked as deleted", req.params.teacherID);
-                return doc;
             });
+            console.log("teacher %s is marked as deleted", req.params.teacherID);
+            return res.json(deletedOne);
         } else {
-            return teachers.remove({ _id: mongoist.ObjectId(req.params.teacherID) }, {
-                justOne: true
-            }).then(function(result) {
-                // result == {"ok":1,"n":1, "deletedCount":1}
-                console.log("teacher %s is deleted", req.params.teacherID);
-                return result;
-            });
+            const result = await db.teachers.remove({ _id: mongoist.ObjectId(req.params.teacherID) }, { justOne: true })
+            // result == {"ok":1,"n":1, "deletedCount":1}
+            console.log("teacher %s is deleted", req.params.teacherID);
+            return res.json(result);
         }
-    }).then(function(result) {
-        return res.json(result);
-    }).catch(function(err) {
+    } catch (err) {
         var error = new Error("Delete teacher fails");
         error.innerError = err;
         return next(error);
-    });
+    }
 });
 
 /**
