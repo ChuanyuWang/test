1aaa3aba9b41ffddfbb0443eec4fb238c24fe57d
Chuanyu Wang <chuanyu.wang@sap.com>
2020-12-07 19:24:05 +0800

    refactor my books page

---
+++ src/js/components/my-read-books.vue
@@ -0,0 +1,199 @@
+<style lang='less'>
+</style>
+
+<template lang="pug">
+div.container(style='padding-left:7px;padding-right:7px')
+  form.form-horizontal(style='margin-top:15px;padding-left:8px;padding-right:8px',v-show='!user._id')
+    div.form-group.form-group-sm
+      div.col-xs-12(style='color:#808080;text-align:center') 请输入宝宝姓名和联系方式查看阅读过的绘本
+    div.form-group.form-group-sm(:class='{"has-error": errors.name}')
+      label.control-label-sm.col-xs-3(for='cls_name') 宝宝姓名:
+      div.col-xs-9
+        input.form-control(type='text',placeholder='宝宝注册时用的姓名',name='name',v-model='name')
+    div.form-group.form-group-sm(:class='{"has-error": errors.contact}')
+      label.control-label-sm.col-xs-3(for='cls_name') 联系方式:
+      div.col-xs-9
+        input.form-control(type='tel',placeholder='135xxx',name='contact',v-model='contact')
+    div.form-group
+      div.col-xs-offset-3.col-xs-9
+        button.btn.btn-primary(@click='handleLoginOK',:disabled='hasError') 登录
+  div#user_info.row(style='margin:7px -7px 3px -7px',v-show='user.name')
+    p.pull-left.col-xs-10 <b>{{user.name}}</b>小朋友
+
+  div#content(v-show='user._id')
+    table.table.table-condensed.table-striped(style='margin-bottom:7px')
+      thead
+        tr
+          th(style='min-width:85px') 日期
+          th 上课绘本
+          th(style='min-width:40px') 老师
+      tbody
+        tr(v-for="(item, i) in books" :key="i")
+          td {{item.date | dateFilter}}
+          td {{item.title}}
+          td {{item.teacher}}
+    p.small 阅读绘本共: <strong>{{books.length}}</strong>册
+  div.alert.alert-warning(role='alert',style='margin-top:7px',v-show='user._id && books.length === 0') <strong>提示: </strong>没有找到绘本
+</template>
+
+<script>
+/**
+ * --------------------------------------------------------------------------
+ * administrator cockpit page include all the admin features, e.g. create tenant and user
+ * --------------------------------------------------------------------------
+ */
+
+var common = require("../common");
+
+module.exports = {
+  name: "my-read-books",
+  props: {},
+  data: function() {
+    return {
+      name: "",
+      contact: "",
+      user: {
+        _id: null,
+        name: ""
+      },
+      classes: []
+    };
+  },
+  watch: {},
+  components: {},
+  computed: {
+    hasData: function() {
+      return !jQuery.isEmptyObject(this.selectedTenant);
+    },
+    errors: function() {
+      var errors = {};
+      if (!this.name) errors.name = "名字不能为空";
+      if (!this.contact) errors.contact = "联系方式不能为空";
+      return errors;
+    },
+    hasError: function() {
+      var errors = this.errors;
+      return Object.keys(errors).some(function(key) {
+        return true;
+      });
+    },
+    books: function() {
+      var allBooks = [];
+      for (var i = 0; i < (this.classes && this.classes.length); i++) {
+        var books = this.classes[i].books || [];
+        for (var j = 0; j < books.length; j++) {
+          // append the date field to each book
+          books[j].date = this.classes[i].date;
+          allBooks.push(books[j]);
+        }
+      }
+
+      return allBooks;
+    }
+  },
+  filters: {
+    dateFilter: function(value) {
+      if (!value) return "";
+
+      return moment(value).format('l');
+    }
+  },
+  methods: {
+    showMyBooks: function() {
+      var vm = this;
+      var query = {
+        memberid: memberid,
+        from: moment(0).toISOString(),
+        to: moment().add(1, 'years').toISOString(),
+        order: 'desc',
+        tenant: common.getTenantName(),
+        hasBooks: true
+      };
+      var request = $.ajax("/api/classes", {
+        type: "GET",
+        //contentType : "application/x-www-form-urlencoded; charset=UTF-8",
+        data: query,
+        dataType: "json"
+      });
+
+      request.fail(function(jqXHR, textStatus, errorThrown) {
+        // handle login error
+        console.error(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
+      });
+      request.done(function(data, textStatus, jqXHR) {
+        vm.classes = data || [];
+      });
+    },
+    handleLoginOK: function(event) {
+      // prevent from submitting the form
+      event.preventDefault();
+
+      // validate the input
+      var userInfo = {
+        tenant: common.getTenantName(),
+        name: this.name,
+        contact: this.contact
+      };
+
+      var vm = this;
+      var request = $.ajax("/api/members/validate", {
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(userInfo),
+        dataType: "json"
+      });
+      request.fail(function(jqXHR, textStatus, errorThrown) {
+        // handle login error
+        console.error(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
+      });
+      request.done(function(data, textStatus, jqXHR) {
+        if (data) {
+          try {
+            // cache the member id in global variable before access localStorage
+            localStorage._memberid = memberid = data._id;
+            localStorage._name = data.name;
+            localStorage._contact = data.contact;
+          } catch (oException) {
+            if (oException.name == 'QuotaExceededError') {
+              console.error('超出本地存储限额！');
+              //clear the local storage
+              localStorage.clear();
+            }
+          }
+          vm.user._id = data._id;
+          vm.user.name = data.name;
+
+          vm.showMyBooks();
+        } else {
+          // handle login fail, show error dialog
+          $('#error_dlg').find("h4").text('登录失败');
+          $('#error_dlg').find("p#message").text('没有找到会员信息，请核对您的姓名和联系方式，如有问题请联系客服');
+          $('#error_dlg').modal('show');
+        }
+      });
+    }
+  },
+  created: function() {
+    var vm = this;
+    var request = $.ajax("api/tenants", {
+      type: "GET",
+      //contentType : "application/x-www-form-urlencoded; charset=UTF-8",
+      dataType: "json",
+      data: {},
+      cache: true // browser cache
+    });
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+      //console.error(jqXHR);
+    });
+    request.done(function(data, textStatus, jqXHR) {
+      //vm.tenants = data;
+    });
+    // initialize the default value from local storage
+    this.name = localStorage._name;
+    this.contact = localStorage._contact;
+  },
+  mounted: function() {
+    //var vm = this;
+  }
+};
+</script>
--- src/js/mybooks.js
+++ src/js/mybooks.js
@@ -3,17 +3,16 @@
  * mybooks.js
  * --------------------------------------------------------------------------
  */
-var common = require('./common');
+var books_app = require('./components/my-read-books.vue').default;
 // open id of Weichat user
 //var _openid = undefined;
-var memberid = undefined;
 
 // DOM Ready =============================================================
-$(document).ready(function () {
+$(document).ready(function() {
     init();
-
-    // click login
-    $('#login .btn').click(handleLoginOK);
+    // register the root component globally
+    Vue.component('my-read-books', books_app);
+    new Vue({ el: '#app' });
 });
 
 // Functions =============================================================
@@ -22,169 +21,4 @@ function init() {
     console.log("welcome~~~");
     moment.locale('zh-CN');
     //bootbox.setLocale('zh_CN');
-    //$('#currentWeekRange').text(moment().format('[今天] MMMDo'));
-    
-    toggleLoginForm(true);
-}
-
-function handleLoginOK(event) {
-    // prevent from submitting the form
-    event.preventDefault();
-
-    var loginForm = $(this).closest('form');
-    var hasError = false;
-    // validate the input
-    var userInfo = {
-        tenant : common.getTenantName()
-    };
-    userInfo.name = loginForm.find('input[name=name]').val().trim();
-    if (!userInfo.name || userInfo.name.length == 0) {
-        loginForm.find('input[name=name]').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        loginForm.find('input[name=name]').closest(".form-group").removeClass("has-error");
-    }
-    // get contact
-    userInfo.contact = loginForm.find('input[name=contact]').val().trim();
-    if (!userInfo.contact || userInfo.contact.length == 0) {
-        loginForm.find('input[name=contact]').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        loginForm.find('input[name=contact]').closest(".form-group").removeClass("has-error");
-    }
-
-    if (!hasError) {
-        $.ajax("/api/members/validate", {
-            type : "POST",
-            contentType: "application/json; charset=utf-8",
-            data : JSON.stringify(userInfo),
-            success : function (data) {
-                if (data) {
-                    try {
-                        // cache the member id in global variable before access localStorage
-                        localStorage._memberid = memberid = data._id;
-                        localStorage._name = data.name;
-                        localStorage._contact = data.contact;
-                    }catch (oException) {
-                        if(oException.name == 'QuotaExceededError'){
-                            console.error('超出本地存储限额！');
-                            //clear the local storage
-                            localStorage.clear();
-                        }
-                    }
-                    toggleLoginForm(false);
-                    updateUserInfo(data);
-                    showMyBooks();
-                } else {
-                    // handle login fail
-                    $('#error_dlg').find("h4").text('登录失败');
-                    $('#error_dlg').find("p#message").text('没有找到会员信息，请核对您的姓名和联系方式，如有问题请联系客服');
-                    $('#error_dlg').modal('show');
-                }
-            },
-            error : function (jqXHR, status, err) {
-                // handle login error
-                console.error(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
-            },
-            complete : function (jqXHR, status) {
-                //TODO
-            },
-            dataType : "json"
-        });
-    }
-}
-
-function updateUserInfo(user) {
-    $('#user_info p').html('<b>' + user.name +'</b>小朋友');
-}
-
-function toggleLoginForm(isShow) {
-    // display the login form for user to input name and contact
-    if (isShow) {
-        $('#user_info').hide();
-        $('#content').hide();
-        // initialize the default value from local storage
-        $('#main form input[name=name]').val(localStorage._name);
-        $('#main form input[name=contact]').val(localStorage._contact);
-        $('#main form').fadeIn(400);
-        $('#main form input[name=name]').focus();
-    } else {
-        $('#main form').hide();
-        $('#user_info').show();
-        $('#content').fadeIn(600);
-    }
-}
-
-function showMyBooks(isHistory, tab_id) {
-    clearBooks();
-
-    var query = {
-        memberid : memberid,
-        from : moment(0).toISOString(),
-        to : moment().add(1, 'years').toISOString(),
-        order : 'desc',
-        tenant: common.getTenantName(),
-        hasBooks : true
-    };
-    $.ajax("/api/classes", {
-        type : "GET",
-        //contentType : "application/x-www-form-urlencoded; charset=UTF-8",
-        data : query,
-        success : function (data) {
-            for (var i = 0; i < data.length; i++) {
-                appendBook(data[i]);
-            }
-            if (!data.length) {
-                displayNoClassWarning();
-            }
-        },
-        error : function (jqXHR, status, err) {
-            console.error(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
-        },
-        complete : function (jqXHR, status) {
-            // update the total number
-            var total = $('#content table tbody tr').length;
-            var pTotal = $('#content p.small');
-            var t = pTotal.html().replace('{0}',total);
-            pTotal.html(t);
-        },
-        dataType : "json"
-    });
-}
-
-// append a book at the last row of table
-function appendBook(item) {
-    // append the content to current active panel
-    var table = $('#content table tbody');
-    
-    if (item && item.books) {
-        for (var i=0;i<item.books.length;i++) {
-            var book = item.books[i];
-            var row_book = ['<tr>',
-                '<td>',
-                moment(item.date).format('l'),
-                '</td>',
-                '<td>',
-                book.title,
-                '</td>',
-                '<td>',
-                book.teacher,
-                '</td>',
-                '</tr>'].join('');
-            table.append(row_book);
-        }
-    }
-}
-
-function displayNoClassWarning() {
-    var list = $('#main');
-
-    // append a warning bar
-    list.append("<div class='alert alert-warning' role='alert' style='margin-top:7px'><strong>提示：</strong>没有找到绘本</div>");
-}
-
-function clearBooks() {
-    // remove all rows in table except headers
-    $('#content table tbody tr').remove();
-    $('.alert-warning').remove();
 }
