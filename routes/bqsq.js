046669633725ae0a7db08c702c18b6bca1a17dc6
Chuanyu Wang <chuanyu.wang@sap.com>
2021-06-02 16:27:10 +0800

    remove deprecated module util

--- routes/bqsq.js
+++ routes/bqsq.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var base = require('./base');
 var wechat = require('wechat');
 var API = require('wechat-api');
-var util = require('../util');
+var util = require('../server/databaseManager');
 
 //[{time:1462976508, openid:"o0uUrv4RGMMiGasPF5bvlggasfGk"}]
 var visited_user_list = new Array();
@@ -28,19 +28,19 @@ var api = null;
 var tenant_db = null;
 
 // all requests to this router will append tenant info
-router.use(function (req, res, next) {
+router.use(function(req, res, next) {
     // add the tenant database to request object for later usage
     if (!tenant_db) {
         var tenant_name = req.baseUrl.split("/")[1];
-        tenant_db = util.connect(tenant_name);
+        tenant_db = util.mongojsDB(tenant_name);
     }
     req.db = tenant_db;
-    
+
     if (!tenant) {
         //load tenant info from database and initialize db and api object
         var tenant_name = req.baseUrl.split("/")[1];
         var config_db = util.connect('config');
-        config_db.collection('tenants').findOne({name:tenant_name}, function(err, doc){
+        config_db.collection('tenants').findOne({ name: tenant_name }, function(err, doc) {
             if (err || !doc) {
                 var error = new Error("tenant is not created");
                 error.status = 404;
@@ -59,33 +59,33 @@ router.use(function (req, res, next) {
     }
 });
 
-router.get('/booking', function (req, res) {
-    var timeKey = parseInt(Date.now()/1000);
+router.get('/booking', function(req, res) {
+    var timeKey = parseInt(Date.now() / 1000);
 
     function findUserOpenID(user) {
         return timeKey - user.time <= 1;
     }
-    
+
     var user = visited_user_list.find(findUserOpenID);
     visited_user_list = []; // important, clear the array
 
     console.log("open booking page with user %j", user);
     res.render('bqsq/booking', {
-        title : '会员约课',
-        counter : counter++,
-        timeKey : timeKey,
-        openid : user ? user.openid : '',
-        classroom : req.tenant.classroom ? req.tenant.classroom : []
+        title: '会员约课',
+        counter: counter++,
+        timeKey: timeKey,
+        openid: user ? user.openid : '',
+        classroom: req.tenant.classroom ? req.tenant.classroom : []
     });
 });
 
-router.post('/api/sendText', function (req, res) {
+router.post('/api/sendText', function(req, res) {
     if (!req.body.openid || !req.body.message) {
         res.status(400).send("Missing param 'openid' or 'message'");
         return;
     }
-    
-    api.sendText(req.body.openid, req.body.message, function (err, result) {
+
+    api.sendText(req.body.openid, req.body.message, function(err, result) {
         if (err) {
             console.error("send text message to %s fails with error %j", req.body.openid, err);
             res.status(500).send(err);
@@ -96,7 +96,7 @@ router.post('/api/sendText', function (req, res) {
     });
 });
 
-router.get('/api/currentuser', function (req, res) {
+router.get('/api/currentuser', function(req, res) {
     if (!req.query.timeKey) {
         res.status(400).send("Missing param 'timeKey'");
         return;
@@ -105,7 +105,7 @@ router.get('/api/currentuser', function (req, res) {
     function findUserOpenID(user) {
         return Math.abs(user.time - timeKey) <= 1;
     }
-    
+
     var user = visited_user_list.find(findUserOpenID);
     console.log("Get the current weixin user %j", user);
     res.json(user || {});
@@ -113,11 +113,11 @@ router.get('/api/currentuser', function (req, res) {
 
 // Weichat =============================================================
 
-router.use('/weixin', wechat(tenant, function (req, res, next) {
+router.use('/weixin', wechat(tenant, function(req, res, next) {
     // 微信输入信息都在req.weixin上
     var message = req.weixin;
     console.log("A weixin message is received: %j", message);
-    
+
     // test getting user info
     if (message.MsgType == 'event' && message.Event == "VIEW") {
         if (message.EventKey.indexOf('http://www.bookingme.cc') > -1) {
@@ -125,7 +125,7 @@ router.use('/weixin', wechat(tenant, function (req, res, next) {
             // user open some page from our web site, we need to cache user's openID for further usage
             visited_user_list.push({
                 time: parseInt(message.CreateTime),
-                openid : message.FromUserName
+                openid: message.FromUserName
             });
             /*
             api.getUser(message.FromUserName, function (err, user) {
@@ -156,42 +156,42 @@ router.use('/weixin', wechat(tenant, function (req, res, next) {
     } else if (message.MsgType == 'event' && message.Event == 'CLICK' && message.EventKey == 'club_intro') {
         // 回复Q馆介绍(图文回复)
         res.reply([{
-                    title : '大Q小q绘本馆',
-                    description : '大Q小q致力于学龄前儿童早期阅读的研究和推广，以绘本为工具，以故事会的形式开拓单一的儿童商业业态，并帮助家长择书育儿。',
-                    picurl : 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCDxZj1kzveRrhpzBB4jO2s1s6D3LvlmTLq5o2icst1YoXHfYZhzjbMUx2xQo97JsHbcL7ydLJusibUw/0?wx_fmt=jpeg',
-                    url : 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713766&idx=1&sn=b877e12afcc7734de6d14920abb942f3'
-                }
-            ]);
+            title: '大Q小q绘本馆',
+            description: '大Q小q致力于学龄前儿童早期阅读的研究和推广，以绘本为工具，以故事会的形式开拓单一的儿童商业业态，并帮助家长择书育儿。',
+            picurl: 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCDxZj1kzveRrhpzBB4jO2s1s6D3LvlmTLq5o2icst1YoXHfYZhzjbMUx2xQo97JsHbcL7ydLJusibUw/0?wx_fmt=jpeg',
+            url: 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713766&idx=1&sn=b877e12afcc7734de6d14920abb942f3'
+        }
+        ]);
         return;
     } else if (message.MsgType == 'event' && message.Event == 'CLICK' && message.EventKey == 'club_zhaobin') {
         // 回复招兵买马(图文回复)
         res.reply([{
-                    title : '大Q小q绘本馆招兵买马',
-                    description : '本司招聘故事会讲师等各类兼职，加入我们的大家庭，为孩子建立一片真善美的天空，为自己保留一片纯净的土壤。',
-                    picurl : 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCBrVmica7VKGc67GJeZ3A3KfoMG1e1I4fPibCb2QDD8KUic8CyJWpsD2u4bD14xkUUz1SdOMnu9vBfyQ/0?wx_fmt=jpeg',
-                    url : 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713725&idx=1&sn=b0016b6893aa768ce9205b9b583a3710'
-                }
-            ]);
+            title: '大Q小q绘本馆招兵买马',
+            description: '本司招聘故事会讲师等各类兼职，加入我们的大家庭，为孩子建立一片真善美的天空，为自己保留一片纯净的土壤。',
+            picurl: 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCBrVmica7VKGc67GJeZ3A3KfoMG1e1I4fPibCb2QDD8KUic8CyJWpsD2u4bD14xkUUz1SdOMnu9vBfyQ/0?wx_fmt=jpeg',
+            url: 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713725&idx=1&sn=b0016b6893aa768ce9205b9b583a3710'
+        }
+        ]);
         return;
     } else if (message.MsgType == 'event' && message.Event == 'CLICK' && message.EventKey == 'club_member') {
         // 回复申请会员(图文回复)
         res.reply([{
-                    title : '大Q小q阅读俱乐部招募会员啦',
-                    description : '用心的绘本平台，丰富的主题课程，天天故事会，让我们一起读书吧！',
-                    picurl : 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCDxZj1kzveRrhpzBB4jO2s1DXgxlJ7ibHUww2SzUyuO1icNWf6TPtNv6yeriaK8vic7bubSykkIqQxm0g/0?wx_fmt=jpeg',
-                    url : 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713754&idx=1&sn=c8f3e32b4cc139adfe75e59923a595dd'
-                }
-            ]);
+            title: '大Q小q阅读俱乐部招募会员啦',
+            description: '用心的绘本平台，丰富的主题课程，天天故事会，让我们一起读书吧！',
+            picurl: 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCDxZj1kzveRrhpzBB4jO2s1DXgxlJ7ibHUww2SzUyuO1icNWf6TPtNv6yeriaK8vic7bubSykkIqQxm0g/0?wx_fmt=jpeg',
+            url: 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713754&idx=1&sn=c8f3e32b4cc139adfe75e59923a595dd'
+        }
+        ]);
         return;
     } else if (message.MsgType == 'event' && message.Event == 'CLICK' && message.EventKey == 'club_collect') {
         // 回复收购绘本(图文回复)
         res.reply([{
-                    title : '大Q小q绘本馆二手绘本征集令!',
-                    description : '想看更多的书？想腾出更多的空间？想把好书分享给其他的小朋友？大Q小q绘本馆，二手绘本征集啦~！！！',
-                    picurl : 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCDXviag8uozkdxreEfsAB5TCDjl3358wWgjFjOknI2EfpN077XmoJxTYvqic4B3hSuTAFZce6D8ibGeQ/0?wx_fmt=jpeg',
-                    url : 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713728&idx=1&sn=32b8fe1d8626bc9970808ddf669499eb'
-                }
-            ]);
+            title: '大Q小q绘本馆二手绘本征集令!',
+            description: '想看更多的书？想腾出更多的空间？想把好书分享给其他的小朋友？大Q小q绘本馆，二手绘本征集啦~！！！',
+            picurl: 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCDXviag8uozkdxreEfsAB5TCDjl3358wWgjFjOknI2EfpN077XmoJxTYvqic4B3hSuTAFZce6D8ibGeQ/0?wx_fmt=jpeg',
+            url: 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713728&idx=1&sn=32b8fe1d8626bc9970808ddf669499eb'
+        }
+        ]);
         return;
     }
     //TODO, only reply the message under development env
--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var base = require('./base');
 var wechat = require('wechat');
 var API = require('wechat-api');
-var util = require('../util');
+var util = require('../server/databaseManager');
 
 //[{time:1462976508, openid:"o0uUrv4RGMMiGasPF5bvlggasfGk"}]
 var visited_user_list = new Array();
@@ -28,11 +28,11 @@ var api = null;
 var tenant_db = null;
 
 // all requests to this router will append tenant info
-router.use(function (req, res, next) {
+router.use(function(req, res, next) {
     // add the tenant database to request object for later usage
     if (!tenant_db) {
         var tenant_name = req.baseUrl.split("/")[1];
-        tenant_db = util.connect(tenant_name);
+        tenant_db = util.mongojsDB(tenant_name);
     }
     req.db = tenant_db;
 
@@ -40,7 +40,7 @@ router.use(function (req, res, next) {
         //load tenant info from database and initialize db and api object
         var tenant_name = req.baseUrl.split("/")[1];
         var config_db = util.connect('config');
-        config_db.collection('tenants').findOne({name:tenant_name}, function(err, doc){
+        config_db.collection('tenants').findOne({ name: tenant_name }, function(err, doc) {
             if (err || !doc) {
                 var error = new Error("tenant is not created");
                 error.status = 404;
@@ -59,33 +59,33 @@ router.use(function (req, res, next) {
     }
 });
 
-router.get('/booking', function (req, res) {
-    var timeKey = parseInt(Date.now()/1000);
+router.get('/booking', function(req, res) {
+    var timeKey = parseInt(Date.now() / 1000);
 
     function findUserOpenID(user) {
         return timeKey - user.time <= 1;
     }
-    
+
     var user = visited_user_list.find(findUserOpenID);
     visited_user_list = []; // important, clear the array
 
     console.log("open booking page with user %j", user);
     res.render('bqsq/booking', {
-        title : '会员约课',
-        counter : counter++,
-        timeKey : timeKey,
-        openid : user ? user.openid : '',
-        classroom : req.tenant.classroom ? req.tenant.classroom : []
+        title: '会员约课',
+        counter: counter++,
+        timeKey: timeKey,
+        openid: user ? user.openid : '',
+        classroom: req.tenant.classroom ? req.tenant.classroom : []
     });
 });
 
-router.post('/api/sendText', function (req, res) {
+router.post('/api/sendText', function(req, res) {
     if (!req.body.openid || !req.body.message) {
         res.status(400).send("Missing param 'openid' or 'message'");
         return;
     }
-    
-    api.sendText(req.body.openid, req.body.message, function (err, result) {
+
+    api.sendText(req.body.openid, req.body.message, function(err, result) {
         if (err) {
             console.error("send text message to %s fails with error %j", req.body.openid, err);
             res.status(500).send(err);
@@ -96,7 +96,7 @@ router.post('/api/sendText', function (req, res) {
     });
 });
 
-router.get('/api/currentuser', function (req, res) {
+router.get('/api/currentuser', function(req, res) {
     if (!req.query.timeKey) {
         res.status(400).send("Missing param 'timeKey'");
         return;
@@ -105,7 +105,7 @@ router.get('/api/currentuser', function (req, res) {
     function findUserOpenID(user) {
         return Math.abs(user.time - timeKey) <= 1;
     }
-    
+
     var user = visited_user_list.find(findUserOpenID);
     console.log("Get the current weixin user %j", user);
     res.json(user || {});
@@ -113,11 +113,11 @@ router.get('/api/currentuser', function (req, res) {
 
 // Weichat =============================================================
 
-router.use('/weixin', wechat(tenant, function (req, res, next) {
+router.use('/weixin', wechat(tenant, function(req, res, next) {
     // 微信输入信息都在req.weixin上
     var message = req.weixin;
     console.log("A weixin message is received: %j", message);
-    
+
     // test getting user info
     if (message.MsgType == 'event' && message.Event == "VIEW") {
         if (message.EventKey.indexOf('http://www.bookingme.cc') > -1) {
@@ -125,7 +125,7 @@ router.use('/weixin', wechat(tenant, function (req, res, next) {
             // user open some page from our web site, we need to cache user's openID for further usage
             visited_user_list.push({
                 time: parseInt(message.CreateTime),
-                openid : message.FromUserName
+                openid: message.FromUserName
             });
             /*
             api.getUser(message.FromUserName, function (err, user) {
@@ -156,12 +156,12 @@ router.use('/weixin', wechat(tenant, function (req, res, next) {
     } else if (message.MsgType == 'event' && message.Event == 'CLICK' && message.EventKey == 'zhaobin') {
         // 回复招兵买马(图文回复)
         res.reply([{
-                    title : '大Q小q绘本馆招兵买马',
-                    description : '本司招聘故事会讲师等各类兼职，加入我们的大家庭，为孩子建立一片真善美的天空，为自己保留一片纯净的土壤。',
-                    picurl : 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCBrVmica7VKGc67GJeZ3A3KfoMG1e1I4fPibCb2QDD8KUic8CyJWpsD2u4bD14xkUUz1SdOMnu9vBfyQ/0?wx_fmt=jpeg',
-                    url : 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713725&idx=1&sn=b0016b6893aa768ce9205b9b583a3710'
-                }
-            ]);
+            title: '大Q小q绘本馆招兵买马',
+            description: '本司招聘故事会讲师等各类兼职，加入我们的大家庭，为孩子建立一片真善美的天空，为自己保留一片纯净的土壤。',
+            picurl: 'https://mmbiz.qlogo.cn/mmbiz/B2YQzKx0nCBrVmica7VKGc67GJeZ3A3KfoMG1e1I4fPibCb2QDD8KUic8CyJWpsD2u4bD14xkUUz1SdOMnu9vBfyQ/0?wx_fmt=jpeg',
+            url: 'http://mp.weixin.qq.com/s?__biz=MzAxODg0MTU5MQ==&mid=502713725&idx=1&sn=b0016b6893aa768ce9205b9b583a3710'
+        }
+        ]);
         return;
     }
     //TODO, only reply the message under development env
