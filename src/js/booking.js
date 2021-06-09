a78db7fe5158ab33c341a0e3d13f35cf9dc9690c
Chuanyu Wang <chuanyu.wang@sap.com>
2021-06-09 19:31:10 +0800

    convert booking page to vue application
    TODO:
    scroll to today
    support classroom filter
    support age filter

--- src/js/booking.js
+++ src/js/booking.js
@@ -5,16 +5,11 @@
  */
 
 var common = require('./common');
-// local cache for class or event
-var cls_cache = {};
+var i18nextplugin = require('./locales/i18nextplugin');
+var booking_app = require('./components/booking.vue').default;
+
 // open id of Weichat user
 var _openid = undefined;
-// current classroom ID
-var classroomID = null;
-// the Monday of query week
-var currentMonday = null;
-// the minimum age
-var minimumAge = null; // default is all
 
 var ua = navigator.userAgent.toLowerCase();
 if (ua.indexOf('micromessenger') > -1) { // we are within wechat
@@ -38,51 +33,9 @@ if (ua.indexOf('micromessenger') > -1) { // we are within wechat
 $(document).ready(function() {
     init();
 
-    $('#book_dlg').on('show.bs.modal', function(event) {
-        var button = $(event.relatedTarget); // Button that triggered the modal
-        var item_id = button.closest('.book-col').data('id');
-        var item = cls_cache[item_id];
-        if (!item) {
-            alert("网络异常，请刷新重试");
-            event.preventDefault();
-            console.error("Can't get the class or event item with id %s", item_id);
-            return;
-        }
-
-        var modal = $(this);
-        modal.find('#quantity').val(1);
-        modal.find('#time').text(moment(item.date).format('MMMDoah:mm'));
-        modal.find('#content').text(item.name);
-        modal.find('#name').val(localStorage._name);
-        modal.find('#contact').val(localStorage._contact);
-        modal.find('#book_ok').data('id', item._id); // pass the selected class id to click handler
-    });
-
-    $('#book_ok').click(handleBookOK);
-
-    $('#previous_week').click(function(event) {
-        $("this").prop("disabled", true);
-        currentMonday.subtract(7, 'days');
-        updateSchedule($("this"));
-    });
-
-    $('#next_week').click(function(event) {
-        $("this").prop("disabled", true);
-        currentMonday.add(7, 'days');
-        updateSchedule($("this"));
-    });
-
-    $('#current_week').click(function(event) {
-        $("this").prop("disabled", true);
-        currentMonday = getMonday(moment());
-        updateSchedule($("this"));
-    });
-    $('div.age-filter li>a').click(function(event) {
-        var label = this.innerText;
-        $('div.age-filter button').html(label + '<span class="caret"/>');
-        minimumAge = $(this).data('years') || null;
-        updateSchedule($("this"));
-    });
+    // load the i18next plugin to Vue
+    Vue.use(i18nextplugin);
+    new Vue({ el: '#app', extends: booking_app });
 });
 
 // Functions =============================================================
@@ -91,8 +44,6 @@ function init() {
     console.log("welcome~~~");
     moment.locale('zh-CN');
     //bootbox.setLocale('zh_CN');
-    //$('#currentWeekRange').text(moment().format('[今天] MMMDo'));
-    currentMonday = getMonday(moment());
 
     // set the height of #main div to enable div scroll bar instead of body scroll bar
     $('#main').height(window.innerHeight - $('#topbar').height() - 2);
@@ -114,280 +65,6 @@ function init() {
     }
     document.addEventListener('touchmove', handleMove, true);
     */
-
-    // handle user change the classroom after set the selected option
-    $('#chooseRoom').change(function(event) {
-        var selectedOptions = $(this).find('option:selected');
-        if (selectedOptions.length == 1) {
-            // update the title
-            document.title = '会员约课-' + selectedOptions.text();
-            classroomID = selectedOptions.val();
-            updateSchedule();
-        }
-    });
-
-    // select the first (default) classroom option.
-    // the classroom dropdown list is hidden when there is only one classroom
-    var selectedClassroom = $('#chooseRoom option:selected');
-    var room = common.getParam('classroom');
-    if (room) {
-        // select the classroom in droplist if there is param 'classroom' in the URL
-        var option_ele = $('#chooseRoom option[value=' + room + ']');
-        if (option_ele.length == 1) {
-            selectedClassroom = option_ele;
-        }
-    }
-    if (selectedClassroom.length == 1) {
-        // set the 'selected' property won't fire 'change' event to select control
-        selectedClassroom.prop('selected', true);
-        // update the title
-        document.title = '会员约课-' + selectedClassroom.text();
-        classroomID = selectedClassroom.val();
-    }
-    updateSchedule();
-}
-
-function getMonday(date) {
-    var _date = moment(date);
-    var dayofweek = _date.day();
-    // the Monday of this week
-    if (dayofweek == 0) { // today is Sunday
-        _date.day(-6);
-    } else {
-        _date.day(1);
-    }
-    //set the time to the very beginning of day
-    _date.hours(0).minutes(0).seconds(0).milliseconds(0);
-    return _date;
-}
-
-function handleBookOK(event) {
-    var modal = $(this).closest('.modal');
-    var item_id = $(this).data('id');
-    var item = cls_cache[item_id];
-    if (!item) {
-        alert("网络异常，请刷新重试");
-        event.preventDefault();
-        console.error("Can't get the class or event item with id %s", item_id);
-        return;
-    }
-    var hasError = false;
-    // validate the input
-    var bookInfo = {
-        classid: item_id
-    };
-    bookInfo.name = modal.find('#name').val().trim();
-    if (!bookInfo.name || bookInfo.name.length == 0) {
-        modal.find('#name').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('#name').closest(".form-group").removeClass("has-error");
-    }
-    // get contact
-    bookInfo.contact = modal.find('#contact').val().trim();
-    if (!bookInfo.contact || bookInfo.contact.length == 0) {
-        modal.find('#contact').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('#contact').closest(".form-group").removeClass("has-error");
-    }
-    // get quantity
-    bookInfo.quantity = parseInt(modal.find('#quantity').val());
-    if (isNaN(bookInfo.quantity) || bookInfo.quantity <= 0) {
-        modal.find('#quantity').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('#quantity').closest(".form-group").removeClass("has-error");
-    }
-
-    if (!hasError) {
-        modal.modal('hide');
-        try {
-            localStorage._name = bookInfo.name;
-            localStorage._contact = bookInfo.contact;
-        } catch (oException) {
-            if (oException.name == 'QuotaExceededError') {
-                console.error('超出本地存储限额！');
-                //clear the local storage
-                localStorage.clear();
-            }
-        }
-        addNewBook(bookInfo);
-    }
-}
-
-function addNewBook(bookInfo) {
-    bookInfo.tenant = common.getTenantName();
-    $.ajax("/api/booking", {
-        type: "POST",
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(bookInfo),
-        success: function(data) {
-            //console.log("book successfully with ", bookInfo);
-            // update cache
-            var classInfo = data['class'];
-            cls_cache[classInfo._id] = classInfo;
-            // TODO, update the button status according to latest return data
-            var remaining = common.classRemaining(classInfo);
-            var book_col = $(".book-col[data-id=" + bookInfo.classid + "]");
-            book_col.find("span").text(remaining < 0 ? 0 : remaining);
-            if (remaining <= 0) {
-                book_col.find("button").removeClass('btn-primary').addClass('btn-danger');
-                book_col.find(".book-btn").text("已满").attr('data-toggle', null).attr('data-target', null);
-            }
-
-            displaySuccess(data['member'], classInfo);
-        },
-        error: function(jqXHR, status, err) {
-            displayError(jqXHR.responseJSON, bookInfo);
-        },
-        dataType: "json"
-    });
-}
-
-function displayError(error, bookInfo) {
-    $('#error_dlg').find("p#message").text(error.message);
-    $('#error_dlg').modal('show');
-}
-
-function displaySuccess(member, classInfo) {
-    var message = "您已预约" + moment(classInfo.date).format('MMMDoah:mm') + "活动，请准时参加";
-    //TODO, support multi membership card
-    var credit = 0;
-    if (member.membership && member.membership.length > 0) {
-        credit = Math.round(member.membership[0].credit * 10) / 10;
-        message += '<br>您还剩余' + credit + '课时';
-    }
-    if (member.membership && member.membership.length > 0) {
-        message += '，有效期至' + moment(member.membership[0].expire).format('ll');
-    }
-
-    $('#success_dlg').find("p#message").html(message);
-    $('#success_dlg').modal('show');
-
-    // send a message to user through weixin
-    if (_openid) {
-        var tenant = common.getTenantName();
-        var msg = {
-            openid: _openid,
-            message: "您已预约" + moment(classInfo.date).format('MMMDoah:mm')
-                + "的课程，请准时参加。\n您还剩余" + credit + "课时",
-            tenant: tenant
-        };
-        $.ajax("/api/sendNotification", {
-            type: "POST",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(msg),
-            success: function(data) {
-                //TODO
-            },
-            error: function(jqXHR, status, err) {
-                console.error(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
-            },
-            dataType: "json"
-        });
-    }
-}
-
-// append a class for booking at the end of page
-function displayClass(item) {
-    var list = $('#main');
-    var date = moment(item.date);
-
-    var tmp = date.format('M/D');
-    var lastRow = list.find('div.class-row:last-child');
-    if (!lastRow || lastRow.find('.date-col p').text().indexOf(tmp) == -1) {
-        // add separator bar
-        list.append('<div class="class-separator"></div>');
-        // append a new class row
-        list.append('<div class="row class-row">' +
-            '<div class="col-xs-2 date-col">' +
-            '<p>' + tmp + '<br><small>' + date.format('ddd') + '</small></p>' +
-            '</div>' +
-            '<div class="col-xs-10 content-col"></div>' +
-            '</div>');
-    }
-
-    // insert a class in last row
-    var cls_col = '<p>' + item.name + '</p>';
-
-    var cls_cost = '';
-    if (item.cost && item.cost > 0) {
-        cls_cost = '<span class="cls-tip"><span class="glyphicon glyphicon-bell"></span>' + item.cost + '课时</span>';
-    } else if (item.courseID) {
-        cls_cost = '';
-    } else {
-        cls_cost = '<span class="cls-free">公益活动</span>';
-    }
-
-    var cls_tip = ['<p class="cls-tip"><span class="glyphicon glyphicon-time"></span>',
-        date.format('HH:mm') + ' ',
-        cls_cost + ' ',
-        getAgeLimit(item),
-        '</p>'].join('');
-
-    var remaining = common.classRemaining(item);
-    if (date < moment().subtract(1, 'hours')) {
-        // the class or event is finished one hours ago
-        var btn_book = '<button class="btn btn-default finish-btn" disabled="disabled">结束</button>';
-        var btn_tip = '';
-    } else if (remaining > 0) {
-        var btn_book = '<button class="btn btn-primary book-btn" data-toggle="modal" data-target="#book_dlg">预约</button>';
-        var btn_tip = '<button class="btn btn-primary remain-btn" disabled="disabled">剩余<span class="badge remain-span">' + remaining + '</span></button>';
-    } else {
-        var btn_book = '<button class="btn btn-danger book-btn">已满</button>';
-        var btn_tip = '<button class="btn btn-danger remain-btn" disabled="disabled">剩余<span class="badge remain-span">0</span></button>';
-    }
-
-    lastRow = list.find('div.class-row:last-child');
-    lastRow.find('.content-col').append(
-        '<div>' +
-        '<div class="cls-col">' +
-        cls_col +
-        cls_tip +
-        '</div>' +
-        '<div class="book-col" data-id="' + item._id + '">' +
-        btn_book +
-        btn_tip +
-        '</div>' +
-        '</div>');
-}
-
-function updateSchedule(control) {
-    clearSchedule();
-    var begin = moment(currentMonday);
-    var end = moment(currentMonday).add(7, 'days');
-    $.ajax("/api/classes", {
-        type: "GET",
-        //contentType : "application/x-www-form-urlencoded; charset=UTF-8",
-        data: {
-            from: begin.toISOString(),
-            to: end.toISOString(),
-            classroom: classroomID || undefined, // 'undefined' field will not append to the URL
-            tenant: common.getTenantName(),
-            minAge: minimumAge || undefined // 'undefined' field will not append to the URL
-        },
-        success: function(data) {
-            for (var i = 0; i < data.length; i++) {
-                //cache every displayed class or event
-                cls_cache[data[i]._id] = data[i];
-                displayClass(data[i]);
-            }
-            if (!data.length) {
-                displayNoClassWarning(begin);
-            }
-            scrollToToday();
-        },
-        error: function(jqXHR, status, err) {
-            console.error(jqXHR.responseText);
-        },
-        complete: function(jqXHR, status) {
-            if (control) {
-                control.prop("disabled", false);
-            }
-        },
-        dataType: "json"
-    });
 }
 
 function scrollToToday() {
@@ -405,35 +82,6 @@ function scrollToToday() {
             scrollTop: position
         }, 1000);
     }
-}
-
-function displayNoClassWarning(Monday) {
-    var list = $('#main');
-    // add separator bar
-    list.append('<div class="class-separator"></div>');
-    // append a warning bar
-    list.append("<div class='alert alert-warning' role='alert' style='margin-top:7px'><strong>提示：</strong>本周没有课程，请查看下一周</div>");
-}
-
-function clearSchedule() {
-    // remove all classes and separators
-    $('#main .class-separator').remove();
-    $('#main .class-row').remove();
-    $('#main .alert-warning').remove();
-}
-
-function getAgeLimit(cls) {
-    var age = cls.age || {};
-    age.min = age.min ? Math.round(age.min / 12 * 10) / 10 : null;
-    age.max = age.max ? Math.round(age.max / 12 * 10) / 10 : null;
-    if (age.min && age.max) {
-        return "年龄" + age.min + "至" + age.max + "岁";
-    } else if (age.min) {
-        return "年龄大于" + age.min + "岁";
-    } else if (age.max) {
-        return "年龄小于" + age.max + "岁";
-    }
-    return "";
 }
 
 function getOpenId(code) {
---
+++ src/js/components/booking.vue
@@ -0,0 +1,408 @@
+<template lang="pug">
+div
+  div.container(style='padding-left:7px;padding-right:7px')
+    div.row(style='margin-left:-7px;margin-right:-7px')
+      img.center-block(:src='tenantConfig.logoPath',style="padding:0px;width:200px")
+    div.row(style='margin:7px -7px 3px -7px')
+      div.btn-group(style='float:left;padding-left:9px')
+        button.btn.btn-default.btn-xs(:disabled='loading',@click='updateSchedule(-7)') 上一周
+        button.btn.btn-default.btn-xs(:disabled='loading',@click='updateSchedule("today")') 本周
+        button.btn.btn-default.btn-xs(:disabled='loading',@click='updateSchedule(7)') 下一周
+      div(style='float:right;padding-right:9px')
+        a.btn.btn-success.btn-xs(href='mybooking') 我的课程
+  div.container(style='padding-left:7px;padding-right:7px;overflow-y:auto')
+    template(v-if='classesByDay.length === 0')
+      div.class-separator
+      div.alert.alert-warning(role='alert',style='margin-top:7px')
+        strong 提示：
+        | 本周没有课程，请查看下一周
+    template(v-for="day in classesByDay")
+      div.class-separator
+      div.row.class-row
+        div.col-xs-2.date-col {{day.date | dateFormatter}}
+          br
+          small {{day.date | weekFormatter}}
+        div.col-xs-10.content-col
+          div(v-for="item in day.classes")
+            div.cls-col
+              p {{item.name}}
+              p.cls-tip
+                span.glyphicon.glyphicon-time
+                | {{item.date | timeFormatter}} 
+                template(v-if='item.cost > 0')
+                  span.glyphicon.glyphicon-bell
+                  | {{item.cost}}课时
+                span.cls-free(v-else) 公益活动
+                |  {{item.age | ageFormatter}}
+            div.book-col
+              button.btn.btn-danger.book-btn(v-if='remainingClass(item) === 0') 已满
+              button.btn.btn-primary.book-btn(v-else,@click='showBookDlg(item, $event)') 预约
+              button.btn.btn-primary.remain-btn(disabled) 剩余
+                span.badge.remain-span {{remainingClass(item)}}
+  modal-dialog(ref='bookDlg',size="small",buttonStyle="success",buttons="confirm",:hasError="hasError",@ok="addNewBook(bookItem)") 预约课程
+    template(v-slot:body)
+      form.form-horizontal
+        div.form-group.form-group-sm(style='margin-bottom:9px')
+          label.control-label-sm.col-xs-3 时间:
+          div.col-xs-9
+            p.form-control-static {{bookItem.date | dateTimeFormatter}}
+        div.form-group.form-group-sm
+          label.control-label-sm.col-xs-3 内容:
+          div.col-xs-9
+            p.form-control-static(style='height:auto') {{bookItem.content}}
+        div.form-group.form-group-sm(:class='{"has-error": errors.name}')
+          label.control-label-sm.col-xs-3 宝宝姓名:
+          div.col-xs-9
+            input.form-control(type='text',placeholder='宝宝注册时用的名称',v-model='bookItem.name')
+        div.form-group.form-group-sm(:class='{"has-error": errors.contact}')
+          label.control-label-sm.col-xs-3 联系方式:
+          div.col-xs-9
+            input.form-control(type='tel',placeholder='135xxx',v-model='bookItem.contact')
+        div.form-group.form-group-sm(:class='{"has-error": errors.quantity}')
+          label.control-label-sm.col-xs-3 人数:
+          div.col-xs-4
+            input.form-control(type='number',min='1',step='1',v-model='bookItem.quantity')
+    template(v-slot:action) 预约
+  modal-dialog(ref='errorDlg',size="small",buttonStyle="danger") 预约失败
+    template(v-slot:body)
+      p {{errorMessage}}
+      p(style='color:#808080')
+        small 客服电话: 
+          a(:href='tenantConfig.contact | tel') {{tenantConfig.contact}}
+        br
+        small 门店地址: 
+          a(:href='tenantConfig.addressLink') {{tenantConfig.address}}
+  modal-dialog(ref='successDlg',size="small",buttonStyle="success") 预约成功
+    template(v-slot:body)
+      p(v-html='successMessage')
+      p(style='color:#808080')
+        small 客服电话: 
+          a(:href='tenantConfig.contact | tel') {{tenantConfig.contact}}
+        br
+        small 门店地址: 
+          a(:href='tenantConfig.addressLink') {{tenantConfig.address}}
+</template>
+
+<script>
+/**
+ * --------------------------------------------------------------------------
+ * booking page
+ * --------------------------------------------------------------------------
+ */
+
+var common = require("../common");
+var class_service = require("../services/classes");
+var modalDialog = require("./modal-dialog.vue").default;
+
+module.exports = {
+  name: "booking-app",
+  props: {},
+  data: function() {
+    return {
+      tenantName: common.getTenantName(),
+      tenantConfig: {},
+      loading: false,
+      errorMessage: "",
+      successMessage: "",
+      items: [],
+      // the Monday of query week
+      currentMonday: this.getMonday(moment()),
+      classroomID: null,
+      minimumAge: null,
+      bookItem: {
+        quantity: 1,
+        date: null,
+        content: "",
+        name: "",
+        contact: "",
+        classid: null
+      }
+    };
+  },
+  components: {
+    "modal-dialog": modalDialog
+  },
+  computed: {
+    classesByDay() {
+      var results = [];
+      this.items.forEach(function(value, index, array) {
+        //var date = moment(value.date).format('M/D');
+        if (results.length > 0) {
+          var last = results[results.length - 1];
+          if (moment(last.date).isSame(value.date, "day")) {
+            last.classes.push(value);
+            return;
+          }
+        }
+        results.push({
+          date: value.date,
+          classes: [value]
+        })
+      });
+      return results;
+    },
+    errors: function() {
+      var errors = {};
+      if (!this.bookItem.name || this.bookItem.name.length === 0)
+        errors.name = '姓名不能为空';
+      if (!this.bookItem.contact || this.bookItem.contact.length === 0)
+        errors.contact = '联系方式不能为空';
+      var quantity = parseInt(this.bookItem.quantity);
+      if (isNaN(quantity) || quantity <= 0)
+        errors.quantity = '人数不能小于1';
+      return errors;
+    },
+    hasError: function() {
+      var errors = this.errors;
+      return Object.keys(errors).some(function(key) {
+        return true;
+      })
+    }
+  },
+  filters: {
+    tel(contact) {
+      // Remove the non-digit character from tel string, e.g. 136-6166-6616 -> 13664666616
+      // And append prefix "tel:"
+      return "tel:" + (contact && contact.replace(/\D/g, ''));
+    },
+    dateFormatter(value) {
+      return moment(value).format('M/D');
+    },
+    weekFormatter(value) {
+      return moment(value).format('ddd');
+    },
+    timeFormatter(value) {
+      return moment(value).format('HH:mm');
+    },
+    dateTimeFormatter(value) {
+      return moment(value).format('MMMDoah:mm');
+    },
+    ageFormatter(value) {
+      var age = value || {};
+      var min = age.min ? Math.round(age.min / 12 * 10) / 10 : null;
+      var max = age.max ? Math.round(age.max / 12 * 10) / 10 : null;
+      if (min && max) {
+        return "年龄" + min + "至" + max + "岁";
+      } else if (min) {
+        return "年龄大于" + min + "岁";
+      } else if (max) {
+        return "年龄小于" + max + "岁";
+      }
+      return "";
+    }
+  },
+  methods: {
+    updateSchedule(delta) {
+      var vue = this;
+      this.loading = true;
+      this.item = [];
+      if (delta === 'today') {
+        this.currentMonday = this.getMonday(moment());
+      } else {
+        this.currentMonday.add(delta || 0, 'day');
+      }
+      var begin = moment(this.currentMonday);
+      var end = moment(this.currentMonday).add(7, 'days');
+
+      var request = class_service.getClasses({
+        from: begin.toISOString(),
+        to: end.toISOString(),
+        classroom: this.classroomID || undefined, // 'undefined' field will not append to the URL
+        tenant: common.getTenantName(),
+        minAge: this.minimumAge || undefined // 'undefined' field will not append to the URL
+      });
+      request.done(function(data, textStatus, jqXHR) {
+        vue.items = data || [];
+        //scrollToToday();
+      });
+      request.fail(function(jqXHR, textStatus, errorThrown) {
+        console.error(jqXHR.responseText);
+      });
+      request.always(function(data, textStatus, jqXHR) {
+        vue.loading = false;
+      });
+    },
+    addNewBook(bookInfo) {
+      var vue = this;
+      try {
+        localStorage._name = bookInfo.name;
+        localStorage._contact = bookInfo.contact;
+      } catch (oException) {
+        if (oException.name == 'QuotaExceededError') {
+          console.error('超出本地存储限额！');
+          //clear the local storage
+          localStorage.clear();
+        }
+      }
+      var request = class_service.addReservation({
+        classid: bookInfo.classid,
+        name: bookInfo.name,
+        contact: bookInfo.contact,
+        quantity: parseInt(bookInfo.quantity)
+      });
+
+      request.done(function(data, textStatus, jqXHR) {
+        var classInfo = data['class'];
+        // update booking data
+        vue.items.some(function(value, index, array) {
+          if (value._id === classInfo._id) {
+            value.booking = classInfo.booking;
+            return true;
+          }
+        });
+        vue.updateSuccessMessage(data['member'], classInfo);
+        vue.$refs.successDlg.show();
+        // send a message to user through weixin
+        if (window._openid) {
+          var tenant = common.getTenantName();
+          var msg = {
+            openid: _openid,
+            message: "您已预约" + moment(classInfo.date).format('MMMDoah:mm')
+              + "的课程，请准时参加。\n您还剩余" + credit + "课时",
+            tenant: tenant
+          };
+          $.ajax("/api/sendNotification", {
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(msg),
+            success: function(data) {
+              //TODO
+            },
+            error: function(jqXHR, status, err) {
+              console.error(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
+            },
+            dataType: "json"
+          });
+        }
+      });
+      request.fail(function(jqXHR, textStatus, errorThrown) {
+        //console.error(jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText);
+        vue.errorMessage = jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText;
+        vue.$refs.errorDlg.show();
+      });
+    },
+    updateSuccessMessage(member, classInfo) {
+      var message = "您已预约" + moment(classInfo.date).format('MMMDoah:mm') + "活动，请准时参加";
+      //TODO, support multi membership card
+      var credit = 0;
+      if (member.membership && member.membership.length > 0) {
+        credit = Math.round(member.membership[0].credit * 10) / 10;
+        message += '<br>您还剩余' + credit + '课时';
+      }
+      if (member.membership && member.membership.length > 0) {
+        message += '，有效期至' + moment(member.membership[0].expire).format('ll');
+      }
+      this.successMessage = message;
+    },
+    showBookDlg(item, event) {
+      if (!item) {
+        alert("网络异常，请刷新重试");
+        event.preventDefault();
+        console.error("Can't get the class or event item with id %s", item_id);
+        return;
+      }
+      this.bookItem.classid = item._id;
+      this.bookItem.date = item.date;
+      this.bookItem.content = item.name;
+      this.bookItem.name = localStorage._name;
+      this.bookItem.contact = localStorage._contact;
+      this.$refs.bookDlg.show();
+    },
+    getMonday(date) {
+      var _date = moment(date);
+      var dayofweek = _date.day();
+      // the Monday of this week
+      if (dayofweek == 0) { // today is Sunday
+        _date.day(-6);
+      } else {
+        _date.day(1);
+      }
+      //set the time to the very beginning of day
+      _date.hours(0).minutes(0).seconds(0).milliseconds(0);
+      return _date;
+    },
+    remainingClass(cls) {
+      return common.classRemaining(cls);
+    }
+  },
+  created: function() {
+    this.tenantConfig = _getTenantConfig();
+  },
+  mounted: function() {
+    this.updateSchedule("today");
+  }
+};
+</script>
+
+<style lang="less">
+.class-row {
+  margin-left: -7px;
+  margin-right: -7px;
+}
+
+.date-col {
+  padding-left: 9px;
+  padding-right: 0px;
+  padding-top: 2px;
+  color: #2b2b2b;
+  font-size: 18px;
+  min-width: 50px;
+  text-align: center;
+  line-height: 1;
+  small {
+    font-size: 12px;
+    color: #808080;
+  }
+}
+
+.content-col {
+  padding-left: 7px;
+  padding-right: 9px;
+}
+
+.cls-col {
+  float: left;
+  width: 80%;
+  min-height: 60px;
+}
+
+.book-col {
+  float: right;
+  width: 20%;
+  max-width: 60px;
+  .finish-btn {
+    float: right;
+    margin-top: 3px;
+  }
+  .cancel-btn {
+    .finish-btn;
+    opacity: 0.5;
+  }
+  .book-btn {
+    .finish-btn;
+    padding-bottom: 3px;
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+  }
+  .remain-btn {
+    font-size: 12px;
+    float: right;
+    padding: 0px;
+    width: 54px;
+    background-color: #f0ad4e !important;
+    border-top-left-radius: 0px;
+    border-top-right-radius: 0px;
+    opacity: 1 !important; // remain button is disabled by default, keep the opacity as 1 for better visual
+    .remain-span {
+      padding: 2px;
+      margin-left: 1px;
+      color: #f0ad4e !important;
+    }
+  }
+}
+
+.cls-tip {
+  color: #808080;
+  font-size: 12px;
+}
+</style>
