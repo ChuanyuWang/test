7b2941cd33bf6a617fc78c855dafe5ccb8369e06
Chuanyu Wang <chuanyu.wang@sap.com>
2021-06-16 18:15:04 +0800

    remove unused code

--- routes/tenant.js
+++ routes/tenant.js
@@ -3,24 +3,8 @@ var router = express.Router();
 var helper = require('../helper');
 
 router.get('/booking', function(req, res, next) {
-    if (!req.tenant || !req.tenant.name) {
-        //tenant not found
-        return next();
-    }
-
-    // get the list of public rooms
-    var all_rooms = req.tenant.classroom || [];
-    var public_rooms = all_rooms.filter(function(value, index, array) {
-        return value.visibility != 'internal';
-    });
-
     res.render('bqsq/booking', {
-        title: '会员约课',
-        contact: req.tenant.contact,
-        tel: helper.getTel(req.tenant.contact),
-        address: req.tenant.address,
-        addressLink: req.tenant.addressLink || '#',
-        classroom: public_rooms
+        title: '会员约课'
     });
 });
 
