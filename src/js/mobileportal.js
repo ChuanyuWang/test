5ea2c18c5afa1daa4995ad3ae46dc2491f79b341
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2021-06-24 20:59:41 +0800

    include jQuery lib

--- src/js/mobileportal.js
+++ src/js/mobileportal.js
@@ -6,6 +6,19 @@
 var i18nextplugin = require('./locales/i18nextplugin');
 var portal_app = require('./components/portal.vue').default;
 
-// load the i18next plugin to Vue
-Vue.use(i18nextplugin);
-new Vue({ el: '#app', extends: portal_app, vuetify: new Vuetify() });
+// DOM Ready =============================================================
+$(document).ready(function() {
+    init();
+
+    // load the i18next plugin to Vue
+    Vue.use(i18nextplugin);
+    new Vue({ el: '#app', extends: portal_app, vuetify: new Vuetify() });
+});
+
+// Functions =============================================================
+
+function init() {
+    console.log("welcome~~~");
+    //moment.locale('zh-CN');
+    //bootbox.setLocale('zh_CN');
+}
