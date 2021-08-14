16e62b5cdda6cb99bb4f47177fa06c09fe98a201
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2021-08-14 21:40:52 +0800

    create classroom vue component

---
+++ src/js/components/classrooms.vue
@@ -0,0 +1,217 @@
+<template lang="pug">
+div
+  div#toolbar.btn-group(role='group')
+    button.btn.btn-success(type='button',@click='addClassroom()') 添加
+  bootstrap-table(ref='classroomTable',:columns='columns',:options='options')
+  p.small(style='margin-top:14px') 
+    |*内部教室不对外开放，会员在自助预约时无法选择内部教室
+  modal-dialog(ref='classroomEditDlg',buttonStyle="success") {{newClassroom ? "添加教室" : "修改教室"}}
+    template(v-slot:body)
+      form.form-horizontal
+        div.form-group(:class='{"has-error": errors.id}')
+          label.control-label.col-sm-2(for='classroom_id') 教室ID:
+          div.col-sm-10
+            input#classroom_id.form-control(type='text',name='id',:disabled='!newClassroom',placeholder='英文字母或数字',v-model='classroomID')
+        div.form-group(:class='{"has-error": errors.name}')
+          label.control-label.col-sm-2(for='classroom_name') 教室名字:
+          div.col-sm-10
+            input#classroom_name.form-control(type='text',name='name',placeholder='教室名字',v-model='classroomName')
+          div.col-sm-offset-2.col-sm-10
+            div.checkbox
+              label
+                input(type='checkbox',name='visibility',value='internal',v-model='internal')
+                |内部教室*
+    template(v-slot:footer)
+      p.small(style='color:#777;float:left;margin-top:7px') *内部教室不对外开放，会员在自助预约时无法选择内部教室
+      button.btn.btn-default(type="button",data-dismiss="modal") 取消
+      button.btn.btn-success(type="button",:disabled='hasError',v-if='newClassroom',@click='addNewClassroom') 添加
+      button.btn.btn-primary(type="button",:disabled='hasError',v-else,@click='editClassroom') 修改
+</template>
+
+<script>
+/**
+ * --------------------------------------------------------------------------
+ * classrooms setting component
+ * --------------------------------------------------------------------------
+ */
+
+var util = require('../services/util');
+var modalDialog = require("./modal-dialog.vue").default;
+
+module.exports = {
+  name: "classroom-setting",
+  props: {},
+  data: function() {
+    return {
+      classroomID: "",
+      classroomName: "",
+      internal: false,
+      newClassroom: true,
+      columns: [
+        {
+          field: "id",
+          title: "教室ID",
+          sortable: true
+        }, {
+          field: "name",
+          title: "教室名称"
+        }, {
+          field: "visibility",
+          title: "内部教室*",
+          formatter: this.visibilityFormatter
+        }, {
+          title: "操作",
+          align: "center",
+          formatter: this.actionFormatter,
+          events: {
+            'click .remove-room': this.removeClassroom,
+            'click .edit-room': this.openEditClassroomDialog
+          }
+        }
+      ],
+      options: {
+        locale: "zh-CN",
+        toolbar: '#toolbar',
+        url: "/api/setting/classrooms",
+        showRefresh: true,
+        striped: true,
+        uniqueId: "id"
+      }
+    };
+  },
+  components: {
+    "modal-dialog": modalDialog,
+    "BootstrapTable": BootstrapTable
+  },
+  computed: {
+    errors: function() {
+      var errors = {};
+      if (!this.classroomID || this.classroomID.length === 0)
+        errors.id = 'ID不能为空';
+      if (!this.classroomName || this.classroomName.length === 0)
+        errors.name = '名称不能为空';
+      return errors;
+    },
+    hasError: function() {
+      var errors = this.errors;
+      return Object.keys(errors).some(function(key) {
+        return true;
+      })
+    }
+  },
+  filters: {},
+  methods: {
+    addClassroom(event) {
+      this.classroomID = null;
+      this.classroomName = "";
+      this.internal = false;
+      this.newClassroom = true;
+      this.$refs.classroomEditDlg.show();
+    },
+    removeClassroom(e, value, row, index) {
+      var vm = this;
+      bootbox.confirm({
+        message: "确定永久删除此教室吗？<br><small>删除后，教室中的课程将无法显示或预约，并且已经预约的课时也不会返还到会员卡中</small>",
+        callback: function(ok) {
+          if (!ok) return;
+
+          $.ajax("/api/setting/classrooms/" + row.id, {
+            type: "DELETE",
+            contentType: "application/json; charset=utf-8",
+            data: {},
+            success: function(data) {
+              if (data && data.n == 1 && data.ok == 1) {
+                vm.$refs.classroomTable.removeByUniqueId(row.id);
+              } else {
+                console.error("remove class room " + row.id + " fails");
+              }
+            },
+            error: function(jqXHR, status, err) {
+              util.showAlert("删除教室失败", jqXHR);
+            },
+            dataType: "json"
+          });
+        },
+        buttons: {
+          confirm: {
+            className: "btn-danger"
+          }
+        }
+      });
+    },
+    openEditClassroomDialog(e, value, row, index) {
+      // edit a classroom
+      this.classroomID = row.id;
+      this.classroomName = row.name;
+      this.internal = row.visibility === 'internal';
+      this.newClassroom = false;
+      this.$refs.classroomEditDlg.show();
+    },
+    visibilityFormatter(value, row, index) {
+      if (value == 'internal') return '是';
+      else return '否';
+    },
+    actionFormatter(value, row, index) {
+      return [
+        '<button type="button" class="edit-room btn btn-primary btn-xs">',
+        '  <span class="glyphicon glyphicon-edit"></span> 修改',
+        '</button>',
+        '<button type="button" style="margin-left:6px" class="remove-room btn btn-danger btn-xs">',
+        '  <span class="glyphicon glyphicon-trash"></span> 删除',
+        '</button>'
+      ].join('');
+    },
+    addNewClassroom() {
+      var vm = this;
+      var room = {};
+      room.id = this.classroomID;
+      room.name = this.classroomName;
+      if (this.internal) {
+        room.visibility = "internal";
+      }
+      // close the dialog
+      this.$refs.classroomEditDlg.hide();
+      $.ajax("/api/setting/classrooms", {
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(room),
+        success: function(data) {
+          vm.$refs.classroomTable.insertRow({ index: 0, row: room });
+        },
+        error: function(jqXHR, status, err) {
+          util.showAlert("添加教室失败", jqXHR);
+        },
+        dataType: "json"
+      });
+    },
+    editClassroom() {
+      var vm = this;
+      var room = {};
+      room.id = this.classroomID;
+      room.name = this.classroomName;
+      if (this.internal) {
+        room.visibility = "internal";
+      }
+      // close the dialog
+      this.$refs.classroomEditDlg.hide();
+      var request = $.ajax("/api/setting/classrooms/" + room.id, {
+        type: "PATCH",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(room),
+        dataType: "json"
+      });
+      request.fail(function(jqXHR, textStatus, errorThrown) {
+        util.showAlert("修改教室失败", jqXHR);
+      });
+      request.done(function(data, textStatus, jqXHR) {
+        vm.$refs.classroomTable.refresh();
+      });
+    }
+  },
+  created: function() { },
+  mounted: function() { }
+};
+</script>
+
+<style lang="less">
+</style>
--- src/js/setting.js
+++ src/js/setting.js
@@ -3,11 +3,11 @@
  * setting.js setting page main entry module
  * --------------------------------------------------------------------------
  */
-var util = require('./services/util');
 var i18nextplugin = require('./locales/i18nextplugin');
 var teach_setting = require('./components/teach-setting.vue').default;
 var users_setting = require('./components/users-setting.vue').default;
 var general_setting = require('./components/general-setting.vue').default;
+var classroom_setting = require('./components/classrooms.vue').default;
 
 // DOM Ready =============================================================
 $(document).ready(function() {
@@ -24,203 +24,14 @@ function init() {
     Vue.use(i18nextplugin);
 
     // bootstrap the general setting tab
-    new Vue({
-        el: '#general-setting', render: function(h) {
-            return h(general_setting);
-        }
-    });
+    new Vue({ el: '#general-setting', extends: general_setting });
 
     // bootstrap the teacher setting tab
-    new Vue({
-        el: '#teacher-setting', render: function(h) {
-            return h(teach_setting);
-        }
-    });
+    new Vue({ el: '#teacher-setting', extends: teach_setting });
 
     // bootstrap the users setting tab
-    new Vue({
-        el: '#users-setting', render: function(h) {
-            return h(users_setting);
-        }
-    });
+    new Vue({ el: '#users-setting', extends: users_setting });
 
-    // initialize the classroom table
-    $('#classroom_table').bootstrapTable({
-        locale: 'zh-CN',
-        url: '/api/setting/classrooms',
-        columns: [{}, {}, {
-            formatter: visibilityFormatter
-        }, {
-            formatter: actionFormatter,
-            events: handleActions
-        }]
-    });
-
-    $('#clsroom_dlg').on('show.bs.modal', function(event) {
-        var button = $(event.relatedTarget); // Button that triggered the modal
-        if (button.data('action') == "add") {
-            // create a new classroom
-            var modal = $(this);
-            modal.find('.modal-title').text("添加教室");
-            modal.find('input[name=id]').val("").prop("disabled", false).closest(".form-group").removeClass("has-error");
-            modal.find('input[name=name]').val("").closest(".form-group").removeClass("has-error");
-
-            modal.find('#add_room').show();
-            modal.find('#edit_room').hide();
-        }
-    });
-
-    $('#add_room').click(handleAddNewClassRoom);
-    $('#edit_room').click(handleEditClassRoom);
+    // bootstrap the classroom setting tab
+    new Vue({ el: '#classroom-setting', extends: classroom_setting });
 }
-
-function handleAddNewClassRoom(event) {
-    var modal = $(this).closest('.modal');
-    var newRoom = {};
-
-    // validate the input
-    var hasError = false;
-    // get the classroom id
-    newRoom.id = modal.find('input[name=id]').val().trim();
-    if (!newRoom.id || newRoom.id.length == 0) {
-        modal.find('input[name=id]').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('input[name=id]').closest(".form-group").removeClass("has-error");
-    }
-    // get the classroom name
-    newRoom.name = modal.find('input[name=name]').val().trim();
-    if (!newRoom.name || newRoom.name.length == 0) {
-        modal.find('input[name=name]').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('input[name=name]').closest(".form-group").removeClass("has-error");
-    }
-
-    // get the classroom visibility
-    newRoom.visibility = modal.find('input[name=visibility]:checked').val();
-
-    if (!hasError) {
-        modal.modal('hide');
-        addNewClassroom(newRoom);
-    }
-}
-
-function handleEditClassRoom(event) {
-    var modal = $(this).closest('.modal');
-    var room = {};
-
-    // validate the input
-    var hasError = false;
-    // get the classroom id
-    room.id = modal.find('input[name=id]').val().trim();
-    // get the classroom name
-    room.name = modal.find('input[name=name]').val().trim();
-    if (!room.name || room.name.length == 0) {
-        modal.find('input[name=name]').closest(".form-group").addClass("has-error");
-        hasError = true;
-    } else {
-        modal.find('input[name=name]').closest(".form-group").removeClass("has-error");
-    }
-
-    // get the classroom visibility
-    room.visibility = modal.find('input[name=visibility]:checked').val();
-
-    if (!hasError) {
-        modal.modal('hide');
-        var request = editClassroom(room);
-        request.done(function(data, textStatus, jqXHR) {
-            $('#classroom_table').bootstrapTable('refresh');
-        });
-    }
-}
-
-function addNewClassroom(room) {
-    $.ajax("/api/setting/classrooms", {
-        type: "POST",
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(room),
-        success: function(data) {
-            $('#classroom_table').bootstrapTable('insertRow', { index: 0, row: room });
-        },
-        error: function(jqXHR, status, err) {
-            util.showAlert("添加教室失败", jqXHR);
-        },
-        dataType: "json"
-    });
-}
-
-function editClassroom(room) {
-    var request = $.ajax("/api/setting/classrooms/" + room.id, {
-        type: "PATCH",
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(room),
-        dataType: "json"
-    });
-    request.fail(function(jqXHR, textStatus, errorThrown) {
-        util.showAlert("修改教室失败", jqXHR);
-    });
-    return request;
-}
-
-function visibilityFormatter(value, row, index) {
-    if (value == 'internal') return '是';
-    else return '否';
-}
-
-function actionFormatter(value, row, index) {
-    return [
-        '<button type="button" class="edit-room btn btn-primary btn-xs">',
-        '  <span class="glyphicon glyphicon-edit"></span> 修改',
-        '</button>',
-        '<button type="button" style="margin-left:6px" class="remove-room btn btn-danger btn-xs">',
-        '  <span class="glyphicon glyphicon-trash"></span> 删除',
-        '</button>'
-    ].join('');
-}
-
-// event handler defined in setting.jade file for removing classroom
-var handleActions = {
-    'click .remove-room': function(e, value, row, index) {
-        bootbox.confirm({
-            message: "确定永久删除此教室吗？<br><small>删除后，教室中的课程将无法显示或预约，并且已经预约的课时也不会返还到会员卡中</small>",
-            callback: function(ok) {
-                if (!ok) return;
-
-                $.ajax("/api/setting/classrooms/" + row.id, {
-                    type: "DELETE",
-                    contentType: "application/json; charset=utf-8",
-                    data: {},
-                    success: function(data) {
-                        if (data && data.n == 1 && data.ok == 1) {
-                            $('#classroom_table').bootstrapTable('removeByUniqueId', row.id);
-                        } else {
-                            console.error("remove class room " + row.id + " fails");
-                        }
-                    },
-                    error: function(jqXHR, status, err) {
-                        util.showAlert("删除教室失败", jqXHR);
-                    },
-                    dataType: "json"
-                });
-            },
-            buttons: {
-                confirm: {
-                    className: "btn-danger"
-                }
-            }
-        });
-    },
-    'click .edit-room': function(e, value, row, index) {
-        // edit a classroom
-        var modal = $('#clsroom_dlg');
-        modal.find('.modal-title').text("修改教室");
-        modal.find('input[name=id]').val(row.id).prop("disabled", true).closest(".form-group").removeClass("has-error");
-        modal.find('input[name=name]').val(row.name).closest(".form-group").removeClass("has-error");
-        modal.find('input[name=visibility]').prop('checked', row.visibility == 'internal');
-        modal.find('#add_room').hide();
-        modal.find('#edit_room').show();
-
-        modal.modal('show');
-    }
-};
