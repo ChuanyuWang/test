ccfe05d8b4d4ceb7523c834ae3afa6a0b628383d
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2021-11-11 21:42:48 +0800

    add common components and modules

---
+++ src/common/common.js
@@ -0,0 +1,73 @@
+/**
+ * --------------------------------------------------------------------------
+ * common.js
+ * --------------------------------------------------------------------------
+ */
+
+module.exports = {
+    /**
+     * get the tenant name of current page, e.g.
+     * return 'bqsq' from http://localhost:3000/t/bqsq/course/1/view
+     */
+    getTenantName: function() {
+        var pathname = window.location.pathname;
+        if (pathname.length == 0) return "";
+        if (pathname.charAt(0) == '/') pathname = pathname.substring(1);
+        if (pathname.charAt(0) == 't') pathname = pathname.substring(1);
+        if (pathname.charAt(0) == '/') pathname = pathname.substring(1);
+        return pathname.split('/')[0];
+    },
+    /**
+     * get the tenant setting, e.g. {feature: 'book'}
+     */
+    getTenantSetting: function() {
+        var settings = {};
+        var el = $('tenant-setting');
+        if (el.length > 0) {
+            settings.feature = el.attr('feature') || 'common' // default is 'common'
+        }
+        return settings;
+    },
+    /**
+     * Data fomatter function of bootstrap-table to format date localized string by 'll'
+     * 
+     * @param {Object} value the field value
+     * @param {Object} row the row record data
+     * @param {Number} index the row index
+     */
+    dateFormatter: function(value, row, index) {
+        if (value) {
+            return moment(value).format('ll');
+        } else {
+            return undefined;
+        }
+    },
+    /**
+     * Calculate the remaining capacity of class object
+     * 
+     * @cItem {Object} cItem class object
+     */
+    classRemaining: function(cItem) {
+        if (cItem) {
+            var booking = cItem.booking || [];
+            if (booking.length === 0) return cItem.capacity || 0;
+            else {
+                var reservation = 0;
+                booking.forEach(function(val, index, array) {
+                    reservation += (val.quantity || 0);
+                });
+                return (cItem.capacity || 0) - reservation;
+            }
+        } else {
+            return undefined;
+        }
+    },
+    /**
+     * Get the parameter from url
+     */
+    getParam: function(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var param = window.location.search.substr(1).match(reg);
+        return param ? decodeURI(param[2]) : null;
+    }
+};
---
+++ src/components/date-picker.vue
@@ -0,0 +1,125 @@
+<template lang="pug">
+div.input-group.date(:id="id",:class="inputClass")
+  input.form-control(type="text",
+        :name="name",
+        :placeholder="placeholder",
+        :required="required",
+        :readOnly="readOnly",
+        :disabled="disabled")
+  span.input-group-addon
+    span.glyphicon.glyphicon-calendar
+</template>
+
+<script>
+/**
+ * --------------------------------------------------------------------------
+ * date-picker.vue component for input date/time
+ * --------------------------------------------------------------------------
+ */
+
+module.exports = {
+  props: {
+    value: {
+      default: null,
+      required: true,
+      validator: function(value) {
+        return value === null || value instanceof Date || typeof value === 'string' || value instanceof String || value instanceof moment
+      }
+    },
+    // http://eonasdan.github.io/bootstrap-datetimepicker/Options/
+    config: {
+      type: Object,
+      default: function() { return { "format": "ll", "locale": "zh-CN" } }
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    inputClass: {
+      type: [String, Object],
+      default: ''
+    },
+    name: {
+      type: String,
+      default: 'datetime'
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    readOnly: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    id: {
+      type: String
+    },
+  },
+  data: function() {
+    return {
+      dp: null,
+    };
+  },
+  mounted: function() {
+    // Return early if date-picker is already loaded
+    if (this.dp) return;
+    // Init date-picker
+    var $elem = $(this.$el);
+    $elem.datetimepicker(this.config);
+    // Store data control
+    this.dp = $elem.data('DateTimePicker');
+    // Set initial value
+    this.dp.date(moment(this.value));
+    // Watch for changes
+    $elem.on('dp.change', this.onChange);
+  },
+  beforeDestroy: function() {
+    $(this.$el).off();
+    // Free up memory
+    if (this.dp) {
+      this.dp.destroy();
+      this.dp = null;
+    }
+  },
+  watch: {
+    /**
+     * Listen to change from outside of component and update DOM
+     *
+     * @param newValue
+     */
+    value: function(newValue) {
+      if (this.dp) {
+        // if the new value is null, clear the input text manually, because date(null) function doesn't clear previous value
+        if (newValue === null) this.dp.clear();
+        else this.dp.date(moment(newValue || null));
+      }
+    },
+    /**
+     * Watch for any change in options and set them
+     *
+     * @param newConfig Object
+     */
+    config: function(newConfig) {
+      this.dp && this.dp.options(Object.assign(this.dp.options(), newConfig));
+    }
+  },
+  methods: {
+    /**
+     * Update v-model upon change triggered by date-picker itself
+     * Or when value property is set
+     *
+     * @param event
+     */
+    onChange: function(event) {
+      this.$emit('input', event.date || null);
+    }
+  }
+};
+</script>
+
+<style lang='less'>
+</style>
