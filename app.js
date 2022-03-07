fe3c60cf7abbe93d6afe5bef6fda51491f918e58
Chuanyu Wang <chuanyu.wang@sap.com>
2022-03-07 14:56:28 +0800

    Enable en locale for server side

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.use(session({
 
 //i18n configuration
 i18n.configure({
-    locales: ['zh'],
+    locales: ['zh', 'en'],
     // where to store json files
     directory: __dirname + '/locales',
     defaultLocale: 'zh',
