b11baf4b9bb41a46660ef3c68ea42d1b6a2f17cf
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2022-03-21 23:00:42 +0800

    add HMAC-SHA256 sign to util module

--- routes/api/lib/util.js
+++ routes/api/lib/util.js
@@ -1,5 +1,7 @@
 
 const md5 = require('MD5');
+const HmacSHA256 = require("crypto-js/hmac-sha256");
+const Hex = require('crypto-js/enc-hex');
 
 exports.generateNonceString = function(length) {
     var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -11,6 +13,12 @@ exports.generateNonceString = function(length) {
     return noceStr;
 };
 
+/**
+ * Sign params with key as MD5
+ * @param {*} params 
+ * @param {*} key 
+ * @returns 
+ */
 exports.sign = function(params, key) {
     let querystring = Object.keys(params).filter(function(key) {
         return params[key] !== undefined && params[key] !== '' && ['pfx', 'partner_key', 'sign', 'key'].indexOf(key) < 0;
@@ -19,4 +27,20 @@ exports.sign = function(params, key) {
     }).join("&") + "&key=" + key;
 
     return md5(querystring).toUpperCase();
+}
+
+/**
+ * Sign params with key as HMAC-SHA256
+ * @param {*} params 
+ * @param {*} key 
+ * @returns 
+ */
+exports.sign2 = function(params, key) {
+    let querystring = Object.keys(params).filter(function(key) {
+        return params[key] !== undefined && params[key] !== '' && ['pfx', 'partner_key', 'sign', 'key'].indexOf(key) < 0;
+    }).sort().map(function(key) {
+        return key + '=' + params[key];
+    }).join("&") + "&key=" + key;
+
+    return Hex.stringify(HmacSHA256(querystring, key)).toUpperCase();
 }
---
+++ test/sign.test.js
@@ -0,0 +1,41 @@
+const util = require("../routes/api/lib/util");
+
+const chai = require("chai");
+
+//enable assertion styles, include Assert, Expect and Should
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should(); //actually enable should style assertions
+
+describe('POST /api/members', function() {
+    before(async function() {
+        // runs once before the first test in this block
+
+    });
+
+    after(async function() {
+
+    });
+
+    it('should sign as MD5', async function() {
+        let params = {
+            appid: "wxe4283737fc91496e",
+            mch_id: "1577616351",
+            return_code: "SUCCESS",
+            return_msg: "OK"
+        };
+        let result = util.sign(params, "12345");
+        expect(result).to.equal("02ED06CE20F9DA8F86475300C91A1237");
+    });
+
+    it('should sign as HMAC-SHA256', async function() {
+        let params = {
+            appid: "wxe4283737fc91496e",
+            mch_id: "1577616351",
+            return_code: "SUCCESS",
+            return_msg: "OK"
+        };
+        let result = util.sign2(params, "12345");
+        expect(result).to.equal("397D5A7E92DA9D1A539E8F551D1BD8B5F0F52ABF2618825FA94DA8AC0A9E8040");
+    });
+});
