212a2ea3591c2792cc3ff6e5b3cf0a6475e7622d
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2022-03-29 00:29:07 +0800

    add action buttons placeholder

--- src/pages/orders/orders.vue
+++ src/pages/orders/orders.vue
@@ -54,6 +54,15 @@ module.exports = {
         }, {
           field: "totalfee",
           title: "金额(元)"
+        }, {
+          //field: "status", // the events will not work when adding duplicate field
+          title: "操作",
+          formatter: this.actionFormatter,
+          events: {
+            'click .delete-order': this.deleteOrder,
+            'click .close-order': this.closeOrder,
+            'click .refund-order': this.refundOrder
+          }
         }
       ],
       options: {
@@ -85,7 +94,7 @@ module.exports = {
     statusFormatter(value, row, index2) {
       switch (value) {
         case "success":
-          return this.$t("order_success");
+          return this.$t("order_success"); // + '<i class="text-success glyphicon glyphicon-ok"></i>';
         case "closed":
           return this.$t("order_closed");
         case "refund":
@@ -102,6 +111,34 @@ module.exports = {
       var goods = value && value.goods_detail || [];
       return goods[0] && goods[0].goods_name;
     },
+    actionFormatter(value, row, index) {
+      /*
+      return [
+        '<a class="delete-order" href="javascript:void(0)" title="删除订单">',
+        '<i class="text-danger glyphicon glyphicon-trash"></i>',
+        '</a>  ',
+        '<a class="close-order" href="javascript:void(0)" title="关闭订单">',
+        '<i class="text-primary glyphicon glyphicon-ban-circle"></i>',
+        '</a>',
+        '<a class="refund-order" href="javascript:void(0)" title="退款">',
+        '<i class="text-muted glyphicon glyphicon-yen"></i>',
+        '</a>'
+      ].join('');
+      */
+      return [
+        '<div class="btn-group btn-group-xs" role="group">',
+        row.status === "open" ? ['<button type="button" class="btn btn-danger delete-order" title="删除订单">',
+          '<span class="glyphicon glyphicon-trash"></span>',
+          '</button>'].join("") : "",
+        row.status === "notpay" ? ['<button type="button" class="btn btn-primary close-order" title="关闭订单">',
+          '<span class="glyphicon glyphicon-ban-circle"></span>',
+          '</button>'].join("") : "",
+        row.status === "success" ? ['<button type="button" class="btn btn-warning refund-order" title="退款">',
+          '<span class="glyphicon glyphicon-yen"></span>',
+          '</button>'].join("") : "",
+        '</div>'
+      ].join('');
+    },
     customQuery(params) {
       // params : {search: "", sort: undefined, order: "asc", offset: 0, limit: 15}
       params.status = this.filter; // add the status filter
@@ -109,6 +146,24 @@ module.exports = {
     },
     refresh() {
       this.$refs.orderTable.refresh();
+    },
+    refundOrder(e, value, row, index) {
+      if (row.status === "success") {
+        //TODO
+        alert("不支持退款");
+      }
+    },
+    closeOrder(e, value, row, index) {
+      if (row.status === "notpay") {
+        //TODO
+        alert("不支持关闭订单");
+      }
+    },
+    deleteOrder(e, value, row, index) {
+      if (row.status === "open") {
+        //TODO
+        alert("不支持删除订单");
+      }
     }
   },
   created() {
