60cbcd1e4aa9a2bb134c82f7511981ea859a1b2e
Chuanyu Wang <chuanyu.wang@sap.com>
2022-09-01 12:24:36 +0800

    Remove unused account field

--- account.js
+++ account.js
@@ -4,7 +4,6 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 var Account = new Schema({
     username: String,
-    password: String,
     tenant: String,
     displayName: String,
     role: String
