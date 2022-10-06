f50efc04cf6d729a682d8b1900b24acbe40880de
Chuanyu Wang <chuanyu.wang@hotmail.com>
2022-10-07 00:07:41 +0800

    add more util methods for service requests

--- src/services/util.js
+++ src/services/util.js
@@ -42,6 +42,35 @@ util.getJSON = function(url, data) {
         console.error(errorMsg);
     });
     return request;
-}
+};
+
+util.postJSON = function(url, data) {
+    return ajax(url, data, "POST");
+};
+
+util.patchJSON = function(url, data) {
+    return ajax(url, data, "PATCH");
+};
+
+util.deleteJSON = function(url, data) {
+    return ajax(url, data, "DELETE");
+};
+
+function ajax(url, data, method) {
+    var request = $.ajax(url, {
+        // method: method, // version added: 1.9.0
+        type: method, // An alias for method
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+        dataType: "json"
+    });
+
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+        // the returned json body should have a message property to indicate the error
+        var errorMsg = jqXHR.responseJSON ? jqXHR.responseJSON.message : jqXHR.responseText;
+        console.error(errorMsg);
+    });
+    return request;
+};
 
 module.exports = util;
