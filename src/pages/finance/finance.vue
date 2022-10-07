466186fbb1c81672adb88020a7c9a66525576ae2
Chuanyu Wang <chuanyu.wang@hotmail.com>
2022-10-07 18:34:10 +0800

    show active page via API

--- src/pages/finance/finance.vue
+++ src/pages/finance/finance.vue
@@ -3,15 +3,16 @@ div
   ul.nav.nav-tabs(role="tablist")
     li(role="presentation")
       a(href="#orders" role="tab" data-toggle="tab") 订单
-    li.active(role="presentation")
+    li(role="presentation")
       a(href="#contracts" role="tab" data-toggle="tab") 合约
   div.tab-content
-    div#contracts.tab-pane.active(role="tabpanel")
+    div#contracts.tab-pane(role="tabpanel")
       contracts-page
     div#orders.tab-pane(role="tabpanel")
       orders-page
 </template>
 <script>
+var common = require('../../common/common');
 var ordersPage = require('./orders.vue').default;
 var contractsPage = require('./contract-overview.vue').default;
 
@@ -25,7 +26,12 @@ module.exports = {
     "orders-page": ordersPage,
     "contracts-page": contractsPage
   },
-  created() { }
+  created() { },
+  mounted() {
+    // default tab is contract overview
+    var showTab = common.getParam("activetab") || "contracts";
+    $('a[href="#' + showTab + '"]').tab('show');
+  }
 }
 </script>
 
