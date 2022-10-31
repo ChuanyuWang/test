5714f7b94f31f7d2cd0d5d3be88bb44f80c2b1fd
Chuanyu Wang <chuanyu.wang@sap.com>
2022-10-31 16:48:26 +0800

    delay 1 second for db connection establishing

--- test/hooks.cjs
+++ test/hooks.cjs
@@ -4,6 +4,8 @@ const app = require("../app");
 // a global setup fixture is execute once and only once in both parallel and serial mode
 exports.mochaGlobalSetup = async function() {
     console.log('--- mocha test started ---');
+    // wait 1 second for database connection establishing
+    await new Promise(r => setTimeout(r, 1000));
 };
 
 // can be async or not
