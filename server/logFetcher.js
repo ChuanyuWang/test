ca1b13f13b27e097add2cc8fed94551ee143b8f5
Chuanyu Wang <chuanyu.wang@hotmail.com>
2023-02-11 22:16:45 +0800

    add dummy log fetcher

---
+++ server/logFetcher.js
@@ -0,0 +1,8 @@
+const LOGS_SCHEMA = "dlketang_logs";
+
+exports.LOGS_SCHEMA = LOGS_SCHEMA;
+exports.checkYesterdayLog = async function(mongoClient) { }
+exports.startNextTask = async function(mongoClient) {
+    console.log(`fetching log...`);
+}
+exports.repeatTask = async function(mongoClient, date) { }
