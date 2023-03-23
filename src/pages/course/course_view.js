56af24b949ebd9a7add89b3ac7a02cc425988d21
Chuanyu Wang <chuanyu.wang@hotmail.com>
2023-03-23 22:47:28 +0800

    replace jQuery ready method

--- src/pages/course/course_view.js
+++ src/pages/course/course_view.js
@@ -4,27 +4,14 @@
  * Entry module of view course page
  * --------------------------------------------------------------------------
  */
-var i18nextplugin = require('../../locales/i18nextplugin');
+var init = require('../../common/init');
 var courseView = require('./course-view.vue').default;
 
 // DOM Ready =============================================================
-$(document).ready(function() {
-    init();
-
+init(function() {
     // bootstrap the course view page
     new Vue({
         el: '#app',
         components: { 'app': courseView }
     });
 });
-
-// Functions =============================================================
-
-function init() {
-    console.log("course_view moudle init...");
-    // load the i18next plugin to Vue
-    Vue.use(i18nextplugin);
-    //TODO, localization 
-    moment.locale('zh-CN');
-    //bootbox.setLocale('zh_CN');
-}
