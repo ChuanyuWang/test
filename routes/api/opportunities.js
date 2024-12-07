d3a56a49de8c4882aebc452866c89fab023ff7c5
Chuanyu Wang <chuanyu.wang@hotmail.com>
2024-12-07 12:25:38 +0800

    remove mongojs from opportunities router

--- routes/api/opportunities.js
+++ routes/api/opportunities.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var mongojs = require('mongojs');
-var helper = require('../../helper');
-const db_utils = require('../../server/databaseManager');
-const { BadRequestError } = require('./lib/basis');
+const express = require('express');
+const router = express.Router();
+const helper = require('../../helper');
+const { ObjectId } = require('mongodb');
+const { BadRequestError, RuntimeError, ParamError } = require('./lib/basis');
 
-var NORMAL_FIELDS = {
+const NORMAL_FIELDS = {
     since: 1,
     name: 1,
     contact: 1,
@@ -15,24 +14,21 @@ var NORMAL_FIELDS = {
     remark: 1
 };
 
-router.post('/', verifyCode, function(req, res, next) {
+router.post('/', verifyCode, async function(req, res, next) {
     if (!req.tenant) {
-        let error = new Error("tenant is not defined");
-        error.status = 400;
-        return next(error);
+        return next(new BadRequestError("tenant nod found"));
     }
     if (!req.body.name || !req.body.contact) {
-        res.status(400).send("Missing param 'name' or 'contact'");
-        return;
+        return next(ParamError("Missing param 'name' or 'contact'"));
     }
 
     let tenantDB = req.db;
-    var query = {
+    let query = {
         name: req.body.name,
         contact: req.body.contact
     };
 
-    var newDoc = {
+    let newDoc = {
         since: new Date(),
         status: req.body.status || "open",
         name: req.body.name,
@@ -42,35 +38,35 @@ router.post('/', verifyCode, function(req, res, next) {
         source: req.body.source
     };
 
-    var opportunities = tenantDB.collection("opportunities");
-    //if the same opportunity is posted twice, the last one will override the previous
-    opportunities.updateOne(query, { $set: newDoc }, { upsert: true }, function(err, result) {
-        if (err) {
-            var error = new Error('Add opportunity fails with error');
-            error.innerError = err;
-            return next(error);
+    try {
+        const opportunities = tenantDB.collection("opportunities");
+        let result = await opportunities.findOneAndUpdate(
+            query,
+            { $set: newDoc },
+            { upsert: true, returnDocument: "after" }
+        );
+
+        if (result.value) {
+            // {"ok":1,"lastErrorObject":{n:1,...},"value":{"index":0,"_id":"577a77651b298d2b68bfb1c6"}}
+            console.log("opportunity is added with result %j", result.lastErrorObject);
+            return res.json(result.lastErrorObject);
         } else {
-            // result is object, e.g.
-            // {"ok":1,"nModified":0,"n":1,"upserted":[{"index":0,"_id":"577a77651b298d2b68bfb1c6"}]}
-            console.log("opportunity is added with result %j", result);
-            res.json(result);
+            return next(new BadRequestError("Fail to add opportunity"));
         }
-    });
+    } catch (error) {
+        return next(new RuntimeError("Add opportunity fails with error", error));
+    }
 });
 
 /// Below APIs are visible to authenticated users only
 router.use(helper.isAuthenticated);
 
 router.get('/', async function(req, res, next) {
-    //let opportunities = req.db.collection("opportunities");
     try {
         let sort = req.query.order == 'asc' ? 1 : -1;
         let offset = parseInt(req.query.offset) || 0;
         let limit = parseInt(req.query.limit) || 100;
         let search = req.query.search || "";
-
-        let tenantDB = await db_utils.connect(req.tenant.name);
-        let opportunities = tenantDB.collection("opportunities");
 
         let query = {};
         if (search) {
@@ -80,6 +76,8 @@ router.get('/', async function(req, res, next) {
                 { 'contact': new RegExp(search, 'i') }
             ];
         }
+
+        const opportunities = req.db.collection("opportunities");
         let cursor = await opportunities.find(query, {
             projection: NORMAL_FIELDS,
             sort: [["since", sort]]
@@ -93,35 +91,29 @@ router.get('/', async function(req, res, next) {
             rows: docs
         });
     } catch (err) {
-        var error = new Error("find opportunities fails");
-        error.innerError = err;
-        return next(error);
+        return next(new RuntimeError("find opportunities fails"));
     }
 });
 
-router.patch('/:opportunityID', helper.requireRole("admin"), function(req, res, next) {
-    var opportunities = req.db.collection("opportunities");
-
+router.patch('/:opportunityID', helper.requireRole("admin"), async function(req, res, next) {
     initDateField(req.body);
 
-    opportunities.update({
-        _id: mongojs.ObjectId(req.params.opportunityID)
-    }, {
-        $set: req.body
-    }, function(err, result) {
-        if (err) {
-            res.status(500).json({
-                'err': err
-            });
-            return;
-        }
-        if (result.n == 1) {
-            console.log("opportunity %s is updated by %j", req.params.opportunityID, req.body);
-            return res.json(result);
-        } else {
+    try {
+        const opportunities = req.db.collection("opportunities");
+        let result = await opportunities.findOneAndUpdate(
+            { _id: ObjectId(req.params.opportunityID) },
+            { $set: req.body },
+            { returnDocument: "after" }
+        );
+        if (!result.value) {
             return next(new BadRequestError(`opportunity ${req.params.opportunityID} not found`));
         }
-    });
+
+        console.log("opportunity %s is updated by %j", req.params.opportunityID, req.body);
+        return res.json(result.value);
+    } catch (error) {
+        return next(new RuntimeError(`Fail to update opportunity ${req.params.opportunityID}`, error));
+    }
 });
 
 router.delete('/:opportunityID', helper.requireRole("admin"), function(req, res, next) {
@@ -131,53 +123,47 @@ router.delete('/:opportunityID', helper.requireRole("admin"), function(req, res,
 
 // make sure the datetime object is stored as ISODate
 function initDateField(item) {
-    var fields = ["birthday", "since"];
-    for (var i = 0; i < fields.length; i++) {
+    let fields = ["birthday", "since"];
+    for (let i = 0; i < fields.length; i++) {
         if (item && item.hasOwnProperty(fields[i])) {
             item[fields[i]] = new Date(item[fields[i]]);
         }
     }
 }
 
-function verifyCode(req, res, next) {
+async function verifyCode(req, res, next) {
     if (req.app.locals.ENV_DEVELOPMENT) {
         // skip code verification if it's development mode
         return next();
     }
 
     if (!req.tenant) {
-        let error = new Error("tenant is not defined");
-        error.status = 400;
-        return next(error);
+        return next(new BadRequestError("tenant is not defined"));
     }
 
     if (!req.body.code || !req.body.contact) {
-        res.status(400).send("Missing param 'code' or 'contact'");
-        return;
+        return next(new ParamError("Missing param 'code' or 'contact'"));
     }
 
-    let tenantDB = req.db;
+    try {
+        let tenantDB = req.db;
+        const sent_code_collection = tenantDB.collection("sent_code");
+        let now = new Date();
+        now.setMinutes(now.getMinutes() - 10);
+        let doc = await sent_code_collection.findOne({
+            phone: req.body.contact,
+            code: req.body.code,
+            sendDate: { $gte: now }
+        });
 
-    var sent_code_collection = tenantDB.collection("sent_code");
-    let now = new Date();
-    now.setMinutes(now.getMinutes() - 10);
-    sent_code_collection.findOne({
-        phone: req.body.contact,
-        code: req.body.code,
-        sendDate: { $gte: now }
-    }, function(err, doc) {
-        if (err) {
-            var error = new Error('Get verify code fails');
-            error.innerError = err;
-            return next(error);
-        }
         if (!doc) {
-            var err = new Error("验证码无效，请重新提交");
-            err.status = 400;
-            return next(err);
+            return next(new BadRequestError("验证码无效，请重新提交"));
+        } else {
+            return next();
         }
-        return next();
-    });
+    } catch (error) {
+        return next(new RuntimeError("Get verify code fails"));
+    }
 }
 
 module.exports = router;
